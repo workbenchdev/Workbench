df64e2e8a4481d9685abcae4fbb04e91deae4dd2
Sonny Piers <sonny@fastmail.net>
2022-02-27 19:02:22 +0100

    Rework the view togglers and use icons

--- src/main.js
+++ src/main.js
@@ -1,10 +1,15 @@
 import GLib from "gi://GLib";
 import { bindtextdomain, textdomain } from "gettext";
+import Gio from "gi://Gio";
 
 import Application from "./application.js";
+import { relativePath } from "./util.js";
 
 GLib.set_prgname("re.sonny.Workbench");
 GLib.set_application_name("Workbench");
+
+const resource = Gio.resource_load(relativePath("./workbench.gresource"));
+resource._register();
 
 export default function main(argv, { version, datadir }) {
   bindtextdomain(
