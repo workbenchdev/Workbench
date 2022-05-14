bd393207d8b00942e528ddac38a3ff0acf4a2d63
Sonny Piers <sonny@fastmail.net>
2022-05-14 23:20:33 +0200

    Stop emitting a Workbench.vapi file

--- src/Compiler.js
+++ src/Compiler.js
@@ -3,7 +3,6 @@ import GLib from "gi://GLib";
 import DBusPreviewer from "./DBusPreviewer.js";
 
 export default function Compiler(data_dir) {
-
   const proxy = DBusPreviewer();
   const code_file = Gio.File.new_for_path(
     GLib.build_filenamev([data_dir, "code.vala"])
@@ -11,24 +10,48 @@ export default function Compiler(data_dir) {
   const module_file = Gio.File.new_for_path(
     GLib.build_filenamev([data_dir, "libworkbenchcode.so"])
   );
-  
+
   async function compile(code) {
-    code_file.replace_contents(code, null, false, Gio.FileCreateFlags.NONE, null);
+    code_file.replace_contents(
+      code,
+      null,
+      false,
+      Gio.FileCreateFlags.NONE,
+      null
+    );
     try {
       module_file.delete(null);
-    } catch {
-    }
-    const valac = Gio.Subprocess.new([
-      "valac", code_file.get_path(), "--hide-internal", "-X", "-shared", "-X", "-fpic",
-      "--library", "workbench", "-o", module_file.get_path(), "--pkg", "gtk4", "--pkg", "gio-2.0"],
-      Gio.SubprocessFlags.NONE);
-      
+      // eslint-disable-next-line no-empty
+    } catch {}
+    const valac = Gio.Subprocess.new(
+      [
+        "valac",
+        code_file.get_path(),
+        "--hide-internal",
+        "-X",
+        "-shared",
+        "-X",
+        "-fpic",
+        "--library",
+        "workbench",
+        "-o",
+        module_file.get_path(),
+        "--pkg",
+        "gtk4",
+        "--pkg",
+        "gio-2.0",
+        "--vapi",
+        "/dev/null",
+      ],
+      Gio.SubprocessFlags.NONE
+    );
+
     valac.wait(null);
-  
+
     if (!valac.get_successful()) return;
-  
+
     proxy.RunSync(module_file.get_path(), "run", "set_builder");
   }
-  
+
   return { compile };
 }
