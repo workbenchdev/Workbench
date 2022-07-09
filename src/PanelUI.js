b21cc0bde501b8fc67bd0f597f8259b3b5133c3a
Sonny Piers <sonny@fastmail.net>
2022-07-09 15:22:41 +0200

    LSP textDocument and inline Blueprint errors (#131)

--- src/PanelUI.js
+++ src/PanelUI.js
@@ -1,8 +1,11 @@
 import Gio from "gi://Gio";
 import GObject from "gi://GObject";
 import GLib from "gi://GLib";
+import Gtk from "gi://Gtk";
+import Pango from "gi://Pango";
 
-import LSPClient, { LSPError } from "./lsp/LSPClient.js";
+import LSPClient from "./lsp/LSPClient.js";
+import { LSPError, diagnostic_severities } from "./lsp/LSP.js";
 import {
   getLanguage,
   settings,
@@ -10,13 +13,31 @@ import {
   disconnect_signals,
   replaceBufferText,
 } from "./util.js";
+
+import { getPid, once } from "./troll/src/util.js";
 import logger from "./logger.js";
 
 const { addSignalMethods } = imports.signals;
 
-export default function PanelUI({ builder, data_dir, term_console }) {
+export default function PanelUI({ builder, data_dir, version, term_console }) {
   let lang;
-  const blueprint = createBlueprintClient({ data_dir });
+
+  const panel = {
+    xml: "",
+  };
+  addSignalMethods(panel);
+
+  const buffer_blueprint = getLanguage("blueprint").document.buffer;
+  const buffer_xml = getLanguage("xml").document.buffer;
+
+  const blueprint = createBlueprintClient({
+    data_dir,
+    panel,
+    buffer: buffer_blueprint,
+  });
+
+  let document_version = 0;
+  prepareSourceView(getLanguage("blueprint").document.source_view);
 
   async function convertToXML() {
     term_console.clear();
@@ -25,17 +46,15 @@ export default function PanelUI({ builder, data_dir, term_console }) {
     let xml;
 
     try {
-      xml = await compileBlueprint(
-        getLanguage("blueprint").document.buffer.text
-      );
+      xml = await compileBlueprint(buffer_blueprint.text);
     } catch (err) {
       if (err instanceof LSPError) {
-        logBluePrintError(err);
+        logBlueprintError(err);
         return;
       }
       throw err;
     }
-    replaceBufferText(getLanguage("xml").document.buffer, xml);
+    replaceBufferText(buffer_xml, xml);
     settings.set_string("ui-lang", "xml");
   }
   const button_ui_export_xml = builder.get_object("button_ui_export_xml");
@@ -50,15 +69,15 @@ export default function PanelUI({ builder, data_dir, term_console }) {
     let blp;
 
     try {
-      blp = await decompileXML(getLanguage("xml").document.buffer.text);
+      blp = await decompileXML(buffer_xml.text);
     } catch (err) {
       if (err instanceof LSPError) {
-        logBluePrintError(err);
+        logBlueprintError(err);
         return;
       }
       throw err;
     }
-    replaceBufferText(getLanguage("blueprint").document.buffer, blp);
+    replaceBufferText(buffer_blueprint, blp);
     settings.set_string("ui-lang", "blueprint");
   }
   const button_ui_export_blueprint = builder.get_object(
@@ -77,11 +96,6 @@ export default function PanelUI({ builder, data_dir, term_console }) {
     "visible",
     GObject.BindingFlags.SYNC_CREATE
   );
-
-  const panel = {
-    xml: "",
-  };
-  addSignalMethods(panel);
 
   const dropdown_ui_lang = builder.get_object("dropdown_ui_lang");
   // TODO: File a bug libadwaita
@@ -119,7 +133,7 @@ export default function PanelUI({ builder, data_dir, term_console }) {
   }
 
   function onUpdate() {
-    update().catch(logBluePrintError);
+    update().catch(logBlueprintError);
   }
 
   function start() {
@@ -144,47 +158,61 @@ export default function PanelUI({ builder, data_dir, term_console }) {
     start();
     onUpdate();
   });
+
   start();
 
-  async function compileBlueprint(text) {
-    if (!blueprint.proc) {
-      blueprint.start();
+  async function setupLSP() {
+    if (blueprint.proc) return;
+    blueprint.start();
 
-      // await blueprint.request("initialize");
-      // Make Blueprint language server cache Gtk 4
-      // to make subsequence call faster (~500ms -> ~3ms)
-      // await blueprint.request("x-blueprintcompiler/compile", {
-      //   text: "using Gtk 4.0;\nusing Adw 1;\nAdwBin {}",
-      // });
-    }
+    // https://microsoft.github.io/language-server-protocol/specifications/lsp/3.17/specification/#initialize
+    await blueprint.request("initialize", {
+      processId: getPid(),
+      clientInfo: {
+        name: "re.sonny.Workbench",
+        version,
+      },
+      // https://microsoft.github.io/language-server-protocol/specifications/lsp/3.17/specification/#clientCapabilities
+      capabilities: {
+        textDocument: {
+          publishDiagnostics: {},
+          "x-blueprintcompiler/publishCompiled": {},
+        },
+      },
+      locale: "en",
+    });
 
-    const { xml, info } = await blueprint.request(
-      "x-blueprintcompiler/compile",
-      {
-        text,
-      }
+    await blueprint.notify("textDocument/didOpen", {
+      textDocument: {
+        uri: "workbench://state.blp",
+        languageId: "blueprint",
+        version: ++document_version,
+        text: buffer_blueprint.text,
+      },
+    });
+  }
+
+  async function compileBlueprint() {
+    await setupLSP();
+
+    await blueprint.notify("textDocument/didChange", {
+      textDocument: {
+        uri: "workbench://state.blp",
+        version: ++document_version,
+      },
+      contentChanges: [buffer_blueprint.text],
+    });
+
+    const [{ xml }] = await once(
+      blueprint,
+      "notification::textDocument/x-blueprintcompiler/publishCompiled"
     );
-
-    if (info.length) {
-      info.forEach(logBluePrintInfo);
-    } else {
-      term_console.clear();
-    }
 
     return xml;
   }
 
   async function decompileXML(text) {
-    if (!blueprint.proc) {
-      blueprint.start();
-
-      // await blueprint.request("initialize");
-      // Make Blueprint language server cache Gtk 4
-      // to make subsequence call faster (~500ms -> ~3ms)
-      // await blueprint.request("x-blueprintcompiler/compile", {
-      //   text: "using Gtk 4.0;\nusing Adw 1;\nAdwBin {}",
-      // });
-    }
+    await setupLSP();
 
     const { blp } = await blueprint.request("x-blueprintcompiler/decompile", {
       text,
@@ -199,21 +227,32 @@ export default function PanelUI({ builder, data_dir, term_console }) {
   return panel;
 }
 
-function logBluePrintError(err) {
+function logBlueprintError(err) {
   GLib.log_structured("Blueprint", GLib.LogLevelFlags.LEVEL_CRITICAL, {
     MESSAGE: `${err.message}`,
     SYSLOG_IDENTIFIER: "re.sonny.Workbench",
   });
 }
 
-function logBluePrintInfo(info) {
+// function logBlueprintInfo(info) {
+//   GLib.log_structured("Blueprint", GLib.LogLevelFlags.LEVEL_WARNING, {
+//     MESSAGE: `${info.line + 1}:${info.col} ${info.message}`,
+//     SYSLOG_IDENTIFIER: "re.sonny.Workbench",
+//   });
+// }
+
+function logBlueprintDiagnostic({ range, message, severity }) {
   GLib.log_structured("Blueprint", GLib.LogLevelFlags.LEVEL_WARNING, {
-    MESSAGE: `${info.line + 1}:${info.col} ${info.message}`,
+    MESSAGE: `Blueprint-${diagnostic_severities[severity]} ${
+      range.start.line + 1
+    }:${range.start.character} to ${range.end.line + 1}:${
+      range.end.character
+    } ${message}`,
     SYSLOG_IDENTIFIER: "re.sonny.Workbench",
   });
 }
 
-function createBlueprintClient({ data_dir }) {
+function createBlueprintClient({ data_dir, panel, buffer }) {
   const file_blueprint_logs = Gio.File.new_for_path(
     GLib.build_filenamev([data_dir, `blueprint-logs`])
   );
@@ -235,10 +274,66 @@ function createBlueprintClient({ data_dir }) {
     logger.debug("blueprint exit");
   });
   blueprint.connect("output", (self, message) => {
-    logger.debug(`blueprint OUT:\n${message}`);
+    logger.debug(`blueprint OUT:\n${JSON.stringify(message)}`);
   });
   blueprint.connect("input", (self, message) => {
-    logger.debug(`blueprint IN:\n${message}`);
+    logger.debug(`blueprint IN:\n${JSON.stringify(message)}`);
   });
+
+  blueprint.connect(
+    "notification::textDocument/publishDiagnostics",
+    (self, params) => {
+      handleDiagnostics(params.diagnostics, buffer);
+    }
+  );
+
   return blueprint;
+}
+
+function prepareSourceView(source_view) {
+  const tag_table = source_view.buffer.get_tag_table();
+  const tag = new Gtk.TextTag({
+    name: "error",
+    underline: Pango.Underline.ERROR,
+  });
+  tag_table.add(tag);
+}
+
+function handleDiagnostics(diagnostics, buffer) {
+  buffer.remove_tag_by_name(
+    "error",
+    buffer.get_start_iter(),
+    buffer.get_end_iter()
+  );
+
+  diagnostics.forEach((d) => handleDiagnostic(d, buffer));
+}
+
+function handleDiagnostic(diagnostic, buffer) {
+  logBlueprintDiagnostic(diagnostic);
+
+  const [start_iter, end_iter] = get_iters_at_range(buffer, diagnostic.range);
+  buffer.apply_tag_by_name("error", start_iter, end_iter);
+}
+
+function get_iters_at_range(buffer, { start, end }) {
+  let start_iter;
+  let end_iter;
+
+  // Blueprint-Error 13:12 to 13:12 Could not determine what kind of syntax is meant here
+  if (start.line === end.line && start.character === end.character) {
+    [, start_iter] = buffer.get_iter_at_line(start.line);
+    start_iter.forward_sentence_end();
+    start_iter.backward_sentence_start();
+    [, end_iter] = buffer.get_iter_at_line(end.line);
+    end_iter.forward_sentence_end();
+  } else {
+    [, start_iter] = buffer.get_iter_at_line_offset(
+      start.line,
+      start.character
+    );
+    [, end_iter] = buffer.get_iter_at_line_offset(end.line, end.character);
+  }
+
+  return [start_iter, end_iter];
 }
