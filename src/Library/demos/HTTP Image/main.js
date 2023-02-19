dfea582f288ad95b4d806054a0ab6f00369ef375
Sonny Piers <sonny@fastmail.net>
2023-02-19 18:58:21 +0100

    Add HTTP Image example

---
+++ src/Library/demos/HTTP Image/main.js
@@ -0,0 +1,34 @@
+import Gtk from "gi://Gtk?version=4.0";
+import Soup from "gi://Soup?version=3.0";
+import GLib from "gi://GLib";
+import Gio from "gi://Gio";
+import GdkPixbuf from "gi://GdkPixbuf";
+
+Gtk.init();
+
+Gio._promisify(Soup.Session.prototype, "send_async", "send_finish");
+Gio._promisify(
+  GdkPixbuf.Pixbuf,
+  "new_from_stream_async",
+  "new_from_stream_finish",
+);
+
+const input_stream = await getInputStream("https://cataas.com/cat");
+const pixbuf = await GdkPixbuf.Pixbuf.new_from_stream_async(input_stream, null);
+const image = new Gtk.Image();
+image.set_from_pixbuf(pixbuf);
+workbench.preview(image);
+
+async function getInputStream(url) {
+  const session = new Soup.Session();
+  const message = new Soup.Message({
+    method: "GET",
+    uri: GLib.Uri.parse(url, GLib.UriFlags.NONE),
+  });
+  const input_stream = await session.send_async(message, null, null);
+  const { status_code, reason_phrase } = message;
+  if (status_code !== 200) {
+    throw new Error(`Got ${status_code}, ${reason_phrase}`);
+  }
+  return input_stream;
+}
