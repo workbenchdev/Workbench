128c1c648504d7cf0c199c0f397a42c71abcb319
SoNiC-HeRE <sriyanshshivam1@gmail.com>
2023-02-24 16:11:07 +0530

    Updated the js file for GTK Switch Demo

---
+++ src/Library/demos/GTK Switch/main.js
@@ -0,0 +1,33 @@
+import Gtk from "gi://Gtk?version=4.0";
+import GObject from "gi://GObject";
+
+const Switch1 = workbench.builder.get_object("one");
+const Switch2 = workbench.builder.get_object("second");
+const Label1 = workbench.builder.get_object("switch_one");
+const Label2 =  workbench.builder.get_object("switch_second");
+
+Switch1.set_active(true);
+Switch2.set_active(false);
+
+function checkone(){
+  if (Switch1.get_active()){
+    console.log("Switch turned on");
+    Label1.label = "Currently in Active State";
+  } else {
+    console.log("Switch turned off");
+    Label1.label = "Currently in Inactive State";
+  }
+}
+
+function checktwo(){
+  if (Switch2.get_active()){
+    console.log("Switch turned on");
+    Label2.label = "Currently in Inactive State";
+  } else {
+    console.log("Switch turned off");
+    Label2.label = "Currently in Active State";
+  }
+}
+
+Switch1.connect("notify::active",checkone);
+Switch2.connect("notify::active",checktwo);
