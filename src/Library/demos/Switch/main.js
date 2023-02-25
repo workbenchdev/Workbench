8939d3c78b20df7b247a25b85dd31c8092d45ddd
Sonny Piers <sonny@fastmail.net>
2023-02-25 17:50:04 +0100

    Improve design and code

--- src/Library/demos/Switch/main.js
+++ src/Library/demos/Switch/main.js
@@ -1,29 +1,15 @@
-import Gtk from "gi://Gtk?version=4.0";
-import GObject from "gi://GObject";
-
 const switch_on = workbench.builder.get_object("switch_on");
 const label_on = workbench.builder.get_object("label_on");
+
 const switch_off = workbench.builder.get_object("switch_off");
 const label_off = workbench.builder.get_object("label_off");
 
-switch_on.set_active(true);
-switch_off.set_active(false);
+switch_on.connect("notify::active", () => {
+  label_on.label = switch_on.active ? "On" : "Off";
+  switch_off.active = !switch_on.active;
+});
 
-function checkone(){
-  if (switch_on.get_active()){
-    label_on.label = "On";
-  } else {
-    label_on.label = "Off";
-  }
-}
-
-function checktwo(){
-  if (switch_off.get_active()){
-    label_off.label = "On";
-  } else {
-    label_off.label = "Off";
-  }
-}
-
-switch_on.connect("notify::active",checkone);
-switch_off.connect("notify::active",checktwo);
+switch_off.connect("notify::active", () => {
+  label_off.label = switch_off.active ? "On" : "Off";
+  switch_on.active = !switch_off.active;
+});
