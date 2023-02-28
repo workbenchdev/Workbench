f495c8c3c8ec2707caf6fa814e98632639598292
AkshayWarrier <aksmen121@gmail.com>
2023-03-01 03:55:35 +0530

    Avatar: Add FileDialog for selecting images

--- src/Library/demos/Avatar/main.js
+++ src/Library/demos/Avatar/main.js
@@ -1,10 +1,28 @@
 import Adw from "gi://Adw";
+import Gtk from "gi://Gtk";
+import Gdk from "gi://Gdk";
 
-const avatar = workbench.builder.get_object("avatar");
-const name = workbench.builder.get_object("name");
+const avatar_image = workbench.builder.get_object("avatar_image");
+const button = workbench.builder.get_object("button");
 
-avatar.set_text("John Doe");
-avatar.set_size(128);
-avatar.set_show_initials(true);
+const dialog = new Gtk.FileDialog();
 
-name.label = avatar.get_text();
+const filter = new Gtk.FileFilter();
+filter.name = "Image files";
+filter.add_pixbuf_formats();
+dialog.default_filter = filter;
+
+function on_open_cb(dialog, res) {
+  try {
+    const file = dialog.open_finish(res);
+    const texture = Gdk.Texture.new_from_file(file);
+    avatar_image.set_custom_image(texture);
+  } catch {
+    return;
+  }
+}
+
+button.connect("clicked", () => {
+  dialog.open(workbench.window, null, on_open_cb);
+});
+
