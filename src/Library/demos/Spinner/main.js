da782e99ad4c8ac7e215d8d3236c57c1a715c9e6
Sriyansh Shivam <96797205+SoNiC-HeRE@users.noreply.github.com>
2023-02-28 18:19:30 +0530

    library: Add Spinner entry (#199)

---
+++ src/Library/demos/Spinner/main.js
@@ -0,0 +1,12 @@
+const button = workbench.builder.get_object("button");
+const spinner = workbench.builder.get_object("spinner");
+
+button.connect("clicked", () => {
+  if (spinner.spinning === true) {
+    button.icon_name = "media-playback-start";
+    spinner.spinning = false;
+  } else {
+    button.icon_name = "media-playback-pause";
+    spinner.spinning = true;
+  }
+});
