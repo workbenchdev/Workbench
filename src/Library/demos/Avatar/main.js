e76beb123b689a6e28894020b153e920c68c65ef
AkshayWarrier <aksmen121@gmail.com>
2023-03-01 16:48:16 +0530

    Avatar: Update main.js

--- src/Library/demos/Avatar/main.js
+++ src/Library/demos/Avatar/main.js
@@ -1,28 +1,31 @@
 import Adw from "gi://Adw";
 import Gtk from "gi://Gtk";
 import Gdk from "gi://Gdk";
+import Gio from "gi://Gio";
 
 const avatar_image = workbench.builder.get_object("avatar_image");
 const button = workbench.builder.get_object("button");
 
-const dialog = new Gtk.FileDialog();
-
-const filter = new Gtk.FileFilter();
-filter.name = "Image files";
+const filter = new Gtk.FileFilter({
+  name: "Images",
+});
 filter.add_pixbuf_formats();
-dialog.default_filter = filter;
 
-function on_open_cb(dialog, res) {
+const dialog = new Gtk.FileDialog({
+  title: "Select an Avatar",
+  modal: true,
+  default_filter: filter,
+});
+
+Gio._promisify(Gtk.FileDialog.prototype, "open", "open_finish");
+
+button.connect("clicked", async () => {
   try {
-    const file = dialog.open_finish(res);
+    const file = await dialog.open(workbench.window, null);
     const texture = Gdk.Texture.new_from_file(file);
     avatar_image.set_custom_image(texture);
   } catch {
     return;
   }
-}
-
-button.connect("clicked", () => {
-  dialog.open(workbench.window, null, on_open_cb);
 });
 
