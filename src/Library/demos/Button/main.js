8f5fc6ebbd5f65349afba79b78c8d63e69dda703
Sonny Piers <sonny@fastmail.net>
2023-03-05 20:12:07 +0100

    cleanup

--- src/Library/demos/Button/main.js
+++ src/Library/demos/Button/main.js
@@ -1,26 +1,23 @@
-const suggested = workbench.builder.get_object("Suggested");
-const destructive = workbench.builder.get_object("Destructive");
-const custom = workbench.builder.get_object("Custom");
-const plus = workbench.builder.get_object("Plus");
-const minus = workbench.builder.get_object("Minus");
-const left = workbench.builder.get_object("Left");
-const right = workbench.builder.get_object("Right");
-const flat = workbench.builder.get_object("Flat");
-const regular = workbench.builder.get_object("Regular");
-const pill = workbench.builder.get_object("Pill");
+const button_ids = [
+  "regular",
+  "flat",
+  "suggested",
+  "destructive",
+  "custom",
+  "disabled",
+  "circular-plus",
+  "circular-minus",
+  "pill",
+  "osd-left",
+  "osd-right",
+];
 
-function handleClick() {
-  console.log("Button was Clicked");
+for (const id of button_ids) {
+  const button = workbench.builder.get_object(id);
+  button.connect("clicked", onClicked);
 }
 
-suggested.connect("clicked", handleClick);
-destructive.connect("clicked", handleClick);
-custom.connect("clicked", handleClick);
-plus.connect("clicked", handleClick);
-minus.connect("clicked", handleClick);
-left.connect("clicked", handleClick);
-right.connect("clicked", handleClick);
-flat.connect("clicked", handleClick);
-custom.connect("clicked", handleClick);
-regular.connect("clicked", handleClick);
-pill.connect("clicked", handleClick);
+function onClicked(button) {
+  console.log(`${button.name} clicked`);
+}
+
