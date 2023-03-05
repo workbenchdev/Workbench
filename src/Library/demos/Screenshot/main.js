098c946ffd4557f0a3eb9be7a6602f85a98d71e4
Sriyansh Shivam <sriyanshshivam1@gmail.com>
2023-03-06 01:38:27 +0530

    Added Screenshot Demo using Libportal

---
+++ src/Library/demos/Screenshot/main.js
@@ -0,0 +1,33 @@
+import Xdp from "gi://Xdp";
+import XdpGtk from "gi://XdpGtk4";
+const { GdkPixbuf, Gio, GLib, GObject, Gst, Gtk, Pango, PangoCairo } =
+  imports.gi;
+
+const Screenshot = workbench.builder.get_object("Screenshot");
+const portal = new Xdp.Portal();
+const parent = XdpGtk.parent_new_gtk(workbench.window);
+
+function _takeScreenshot() {
+  let flags = Xdp.ScreenshotFlags.NONE;
+
+  portal.take_screenshot(parent, flags, null, (portal, result) => {
+    let path = null;
+
+    try {
+      const uri = this._portal.take_screenshot_finish(result);
+      const [path] = GLib.filename_from_uri(uri);
+      const pixbuf = GdkPixbuf.Pixbuf.new_from_file_at_scale(
+        path,
+        60,
+        40,
+        true,
+      );
+      portal.image.set_from_pixbuf(pixbuf);
+    } catch (e) {
+      logError(e);
+      return;
+    }
+  });
+}
+
+Screenshot.connect("clicked", _takeScreenshot);
