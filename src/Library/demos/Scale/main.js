3ba4ba3c83c060ce9f68d6e70afed3ca89ea056a
Sriyansh Shivam <sriyanshshivam1@gmail.com>
2023-03-08 18:07:10 +0530

    Applied Suggested Changes

--- src/Library/demos/Scale/main.js
+++ src/Library/demos/Scale/main.js
@@ -1,6 +1,17 @@
 const scale_one = workbench.builder.get_object("one");
 const scale_two = workbench.builder.get_object("two");
-const scale_disabled = workbench.builder.get_object("disabled");
+
+function logMarks(scale_value_two) {
+  const marks = {
+    25: "Mark A Reached",
+    50: "Mark B Reached",
+    75: "Mark C Reached",
+    100: "Mark D Reached",
+  };
+  if (scale_value_two in marks) {
+    console.log(marks[scale_value_two]);
+  }
+}
 
 scale_two.add_mark(25.0, "left", "A");
 scale_two.add_mark(50.0, "right", "B");
@@ -8,26 +19,16 @@ scale_two.add_mark(75.0, "right", "C");
 scale_two.add_mark(100.0, "right", "D");
 scale_two.set_increments(25.0, 25.0);
 
-scale_disabled.set_show_fill_level(25);
-
 scale_one.connect("value-changed", () => {
   let scale_value = scale_one.get_value();
   if (scale_value === scale_one.adjustment.upper) {
-    console.log("Max Value Reached");
+    console.log("Maximum Value Reached");
   } else if (scale_value === scale_one.adjustment.lower) {
-    console.log("Min Value Reached");
+    console.log("Minimum Value Reached");
   }
 });
 
 scale_two.connect("value-changed", () => {
   let scale_value_two = scale_two.get_value();
-  if (scale_value_two === 25) {
-    console.log("Mark A Reached");
-  } else if (scale_value_two === 50) {
-    console.log("Mark B Reached");
-  } else if (scale_value_two === 75) {
-    console.log("Mark C Reached");
-  } else if (scale_value_two === 100) {
-    console.log("Mark D Reached");
-  }
+  logMarks(scale_value_two);
 });
