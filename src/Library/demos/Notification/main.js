7c4017d38adad9c964198fecf080c6e950dfdc73
Aryan Bansod <aryanem09@gmail.com>
2023-03-14 10:13:11 +0530

    optimization in code for notifications by shorthand notation

--- src/Library/demos/Notification/main.js
+++ src/Library/demos/Notification/main.js
@@ -3,37 +3,30 @@ import Gio from "gi://Gio";
 const { application, builder } = workbench;
 
 // https://gjs-docs.gnome.org/gio20/gio.notification
-const notification = new Gio.Notification();
-
-notification.set_title("Lunch is ready");
-notification.set_body(
-  "Today we have pancakes and salad, and fruit and cake for dessert",
-);
-notification.set_default_action("app.notification-reply");
-notification.add_button("Accept", "app.notification-accept");
-notification.add_button("Decline", "app.notification-decline");
-
-const icon = new Gio.ThemedIcon({ name: "object-rotate-right-symbolic" });
-notification.set_icon(icon);
+const notification = new Gio.Notification({
+  title: "Lunch is ready",
+  body: "Today we have pancakes and salad, and fruit and cake for dessert",
+  default_action: "app.notification-reply",
+  buttons: [
+    { label: "Accept", action: "app.notification-accept" },
+    { label: "Decline", action: "app.notification-decline" },
+  ],
+  icon: new Gio.ThemedIcon({ name: "object-rotate-right-symbolic" }),
+});
 
 builder.get_object("button_simple").connect("clicked", () => {
   application.send_notification("lunch-is-ready", notification);
 });
 
-const action_reply = new Gio.SimpleAction({ name: "notification-reply" });
-action_reply.connect("activate", () => {
-  console.log("Reply");
-});
-application.add_action(action_reply);
+const actions = [
+  { name: "notification-reply", callback: () => console.log("Reply") },
+  { name: "notification-accept", callback: () => console.log("Accept") },
+  { name: "notification-decline", callback: () => console.log("Decline") },
+];
 
-const action_accept = new Gio.SimpleAction({ name: "notification-accept" });
-action_accept.connect("activate", () => {
-  console.log("Accept");
-});
-application.add_action(action_accept);
-
-const action_decline = new Gio.SimpleAction({ name: "notification-decline" });
-action_decline.connect("activate", () => {
-  console.log("Decline");
-});
+for (const { name, callback } of actions) {
+  const action = new Gio.SimpleAction({ name });
+  action.connect("activate", callback);
+  application.add_action(action);
+}
 application.add_action(action_decline);
