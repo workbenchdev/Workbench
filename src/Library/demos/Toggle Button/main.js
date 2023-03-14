a807b250668bfcb5e561694a91ccbc54ba2db530
Sriyansh Shivam <sriyanshshivam1@gmail.com>
2023-03-15 02:00:21 +0530

    Improved JS Code

--- src/Library/demos/Toggle Button/main.js
+++ src/Library/demos/Toggle Button/main.js
@@ -1,24 +1,13 @@
-const third = workbench.builder.get_object("button_third");
-const fourth = workbench.builder.get_object("button_fourth");
+const camera = workbench.builder.get_object("button_third");
+const flashlight = workbench.builder.get_object("button_fourth");
 const terminal = workbench.builder.get_object("console");
 
-let toggle_count_camera = 0;
-let toggle_count_flashlight = 0;
-let toggle_count_terminal = 0;
-
-third.connect("toggled", () => {
-  toggle_count_camera++;
-  console.log(toggle_count_camera % 2 !== 0 ? "Camera On" : "Camera Off");
+camera.connect("toggled", () => {
+  console.log(camera.get_active() ? "Camera On" : "Camera Off");
 });
-fourth.connect("toggled", () => {
-  toggle_count_flashlight++;
-  console.log(
-    toggle_count_flashlight % 2 !== 0 ? "Flashlight On" : "Flashlight Off",
-  );
+flashlight.connect("toggled", () => {
+  console.log(flashlight.get_active() ? "Flashlight On" : "Flashlight Off");
 });
 terminal.connect("toggled", () => {
-  toggle_count_terminal++;
-  console.log(
-    toggle_count_terminal % 2 !== 0 ? "Entered Console" : "Exited Console",
-  );
+  console.log(terminal.get_active() ? "Entered Console" : "Exited Console");
 });
