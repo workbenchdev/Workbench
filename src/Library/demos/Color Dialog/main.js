52d793571ace8d4b74a4fa49b496835747eb7ed6
AkshayWarrier <aksmen121@gmail.com>
2023-03-15 19:21:55 +0530

    librar: Add Color Dialog entry

---
+++ src/Library/demos/Color Dialog/main.js
@@ -0,0 +1,42 @@
+import Gtk from "gi://Gtk";
+import Gdk from "gi://Gdk";
+import Gio from "gi://Gio";
+
+Gio._promisify(Gtk.ColorDialog.prototype, "choose_rgba", "choose_rgba_finish");
+
+const color_dialog_button = workbench.builder.get_object("color_dialog_button");
+const custom_button = workbench.builder.get_object("custom_button");
+
+const color = new Gdk.RGBA();
+color.parse("white");
+
+const dialog_standard = new Gtk.ColorDialog({
+  title: "Select a color",
+  modal: true,
+  with_alpha: true,
+});
+
+color_dialog_button.dialog = dialog_standard;
+color_dialog_button.rgba = color;
+
+color_dialog_button.connect("notify::rgba", () => {
+  console.log(
+    `Color Dialog Button: The color selected is ${color_dialog_button.rgba.to_string()}`,
+  );
+});
+
+const dialog_custom = new Gtk.ColorDialog({
+  title: "Select a color",
+  modal: true,
+  with_alpha: false,
+});
+
+custom_button.connect("clicked", async () => {
+  try {
+    const color = await dialog_custom.choose_rgba(workbench.window, null, null);
+    console.log(`Custom Buton: The color selected is ${color.to_string()}`);
+  } catch (err) {
+    logError(err);
+  }
+});
+
