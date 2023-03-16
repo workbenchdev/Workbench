b56cb8b1f00b2a57e47c25d2d3d5bd0399461d88
CyberBreach <shrivastavapiyush781@gmail.com>
2023-03-16 18:22:27 +0530

    library: Add Slider Entry.

--- src/Library/demos/Slider/main.js
+++ src/Library/demos/Slider/main.js
@@ -7,3 +7,13 @@ slider.connect("value-changed", () => {
   const value = slider.get_value();
   label.set_text(`Value: ${value}`);
 });
+
+function main() {
+  Gtk.init();
+  const builder = Gtk.Builder.new_from_resource("./main.blp");
+  const window = builder.get_object("window");
+  window.show_all();
+  Gtk.main();
+}
+
+main();
