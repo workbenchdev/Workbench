a00ab48700f9b47d5e603dd885dd7fe0b86fe744
Sriyansh Shivam <sriyanshshivam1@gmail.com>
2023-03-20 05:43:59 +0530

    Applied Suggested Changes

--- src/Library/demos/Calendar/main.js
+++ src/Library/demos/Calendar/main.js
@@ -1,27 +1,31 @@
 const calendar = workbench.builder.get_object("calendar");
+const mark = workbench.builder.get_object("mark");
+const remove_mark = workbench.builder.get_object("remove_mark");
 
-function handleChange() {
-  let year = calendar.year;
-  let month = calendar.month;
-  let day = calendar.day;
-  let monthString = month.toString().padStart(2, "0");
-  let dayString = day.toString().padStart(2, "0");
+calendar.connect("notify::day", () => {
+  console.log(`Selected Date: ${calendar.get_date().format("%x")}`);
+});
 
+calendar.connect("notify::month", () => {
+  console.log(`Switched to Month: ${calendar.get_date().format("%B")}`);
+});
+
+calendar.connect("notify::year", () => {
+  console.log(`Switched to Year: ${calendar.get_date().format("%Y")}`);
+});
+
+mark.connect("clicked", () => {
+  const selectedDate = calendar.get_date().format("%d");
+  calendar.mark_day(selectedDate);
+  console.log("Date Marked");
+});
+
+remove_mark.connect("clicked", () => {
+  const selectedDate = calendar.get_date().format("%d");
   console.log(
-    `Selected Date (YYYY-MM-DD): ${year}-${monthString}-${dayString}`,
+    calendar.get_day_is_marked(selectedDate)
+      ? "Date Unmarked"
+      : "Date not yet marked",
   );
-}
-
-calendar.connect("day-selected", handleChange);
-calendar.connect("next-month", () => {
-  console.log("Switched to Next Month");
-});
-calendar.connect("next-year", () => {
-  console.log("Switched to Next Year");
-});
-calendar.connect("prev-month", () => {
-  console.log("Switched to Previous Month");
-});
-calendar.connect("prev-year", () => {
-  console.log("Switched to Previous Year");
+  calendar.unmark_day(selectedDate);
 });
