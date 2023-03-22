8407e3da35a52724d56a9147be28a9ab006ee4ee
bazylevnik0 <bazylevnik0@gmail.com>
2023-03-22 14:12:33 +0300

    add pc-player and change style

--- src/Library/demos/Grid/main.js
+++ src/Library/demos/Grid/main.js
@@ -15,14 +15,32 @@ for (const id of button_ids) {
   button.connect("clicked", onClicked);
 }
 
+let step = 0;
 function onClicked(button) {
-  if (button.label === "#") {
-    button.label = "x";
-  } else {
-    button.label = "o";
+  //check access for user action
+  if (button.icon_name === "edit") {
+    //store and show user action
+    button.icon_name = "cross-large-symbolic";
+    button.name = "cross";
+    //calculate pc reaction
+    let pc_is_thinking = true;
+    let pc_is_thinking_row;
+    let pc_is_thinking_col;
+    while (pc_is_thinking) {
+      pc_is_thinking_row = "" + Math.floor(Math.random() * 3);
+      pc_is_thinking_col = "" + Math.floor(Math.random() * 3);
+      //make pc reaction if possible
+      let temp = workbench.builder.get_object(
+        "button" + pc_is_thinking_row + pc_is_thinking_col,
+      );
+      if (temp.name !== "cross" && temp.name !== "circle") {
+        temp.icon_name = "circle-outline-thick-symbolic";
+        temp.name = "circle";
+        pc_is_thinking = false;
+        step += 2;
+      }
+      if (step >= 8) pc_is_thinking = false;
+    }
   }
-  console.log(
-    `button${button.name} clicked and now have label '${button.label}'`,
-  );
 }
 
