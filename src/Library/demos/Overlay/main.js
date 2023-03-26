ba7cbad35de63489b4f81db959653eaf11b0f9d9
AkshayWarrier <aksmen121@gmail.com>
2023-03-26 11:48:11 +0530

    Minor change

--- src/Library/demos/Overlay/main.js
+++ src/Library/demos/Overlay/main.js
@@ -5,6 +5,6 @@ import GLib from "gi://GLib";
 const file = Gio.File.new_for_path("image.png");
 
 const picture = workbench.builder.get_object("picture");
-picture.set_file(file);
+picture.file = file;
 
 const toolbar = workbench.builder.get_object("toolbar");
