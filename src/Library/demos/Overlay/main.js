d52ef244bf936b9101e92e0c998541e7c9b3271b
AkshayWarrier <aksmen121@gmail.com>
2023-03-26 19:08:10 +0530

    Overlay: Changes from PR review

--- src/Library/demos/Overlay/main.js
+++ src/Library/demos/Overlay/main.js
@@ -2,9 +2,10 @@ import Gtk from "gi://Gtk";
 import Gio from "gi://Gio";
 import GLib from "gi://GLib";
 
-const file = Gio.File.new_for_path("image.png");
+const file = Gio.File.new_for_path(pkg.pkgdatadir).resolve_relative_path(
+  "Library/demos/Overlay/image.png",
+);
 
 const picture = workbench.builder.get_object("picture");
 picture.file = file;
 
-const toolbar = workbench.builder.get_object("toolbar");
