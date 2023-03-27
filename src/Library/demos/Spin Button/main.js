158b470f18c1bb79c2a4e4b70d6e6eec53cb51ab
kodecheff <akunneemeka125@gmail.com>
2023-03-27 14:14:32 +0100

    SpinButton library update

--- src/Library/demos/Spin Button/main.js
+++ src/Library/demos/Spin Button/main.js
@@ -1,48 +1,7 @@
-import Gtk from "gi://Gtk?version=4.0";
-import GObject from "gi://GObject";
+const spinbutton = workbench.builder.get_object("spinbutton");
 
-Gtk.init();
-
-const open_spin_box = workbench.builder.get_object("open_spinbox");
-
-// Create new Gtk instances
-const spin_button = new Gtk.SpinButton();
-const adjustment = new Gtk.Adjustment({
-  lower: 0,
-  page_increment: 1,
-  step_increment: 1,
-  upper: 100,
-  value: 0,
-});
-
-spin_button.set_adjustment(adjustment);
-
-const Application = GObject.registerClass(
-  {},
-  class extends Gtk.Application {
-    vfunc_activate() {
-      const window = new Gtk.ApplicationWindow({ application: this });
-      const box = new Gtk.Box({
-        orientation: Gtk.Orientation.VERTICAL,
-        marginTop: 36,
-        marginBottom: 36,
-        marginStart: 36,
-        marginEnd: 36,
-        spacing: 16,
-      });
-
-      box.append(spin_button);
-
-      window.child = box;
-      window.set_default_size(400, 200);
-      window.present();
-    }
-  },
-);
-
-const app = new Application();
-
-open_spin_box.connect("clicked", () => {
-  app.vfunc_activate();
+spinbutton.connect("value-changed", (button) => {
+  // Log new entry to the console
+  console.log("New entry:", button.get_value());
 });
 
