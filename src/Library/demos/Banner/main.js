b7d5dd1c9029529f84fb780f2cc44494904659e5
kodecheff <akunneemeka125@gmail.com>
2023-03-28 12:58:49 +0100

    Banner library update

--- src/Library/demos/Banner/main.js
+++ src/Library/demos/Banner/main.js
@@ -1,42 +1,16 @@
-import Gtk from "gi://Gtk?version=4.0";
 const Adw = imports.gi.Adw;
-Gtk.init();
 
-const _bannerBox = workbench.builder.get_object("banner_box");
-const _openBanner = workbench.builder.get_object("open_banner");
+const banner = workbench.builder.get_object("banner");
+const overlay = workbench.builder.get_object("overlay");
 
-function adwBanner() {
-  // Create a new AdwBanner widget
-  const banner = new Adw.Banner({
-    button_label: "OK",
-    title: "Simple Banner Test",
-    revealed: true,
-    use_markup: true,
-    margin_bottom: 20,
+function alert() {
+  const toast = new Adw.Toast({
+    title: "Troubleshoot successful!",
+    timeout: 3,
   });
 
-  return banner;
+  overlay.add_toast(toast);
 }
 
-const banner = adwBanner();
-
-// Signal to open banner
-_openBanner.connect("clicked", () => {
-  if (!banner.revealed) {
-    banner.revealed = true;
-  }
-  _bannerBox.prepend(banner);
-  _bannerBox.remove(_openBanner);
-});
-
 // signal called when banner button is clicked
-banner.connect("button-clicked", async (banner) => {
-  await banner.set_revealed(false);
-
-  if (!banner.revealed) {
-    _bannerBox.remove(banner);
-  }
-
-  _bannerBox.append(_openBanner);
-});
-
+banner.connect("button-clicked", alert);
