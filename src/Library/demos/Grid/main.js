810fadfe75bfd6fc5de85e4c7386b465d7c73d49
Sonny Piers <sonny@fastmail.net>
2023-04-02 15:31:17 +0200

    remove useless else

--- src/Library/demos/Grid/main.js
+++ src/Library/demos/Grid/main.js
@@ -19,29 +19,25 @@ let step = 1;
 function onClicked(button) {
   //check access for user action
   let image = button.get_child();
-  if (image.icon_name) {
-    return;
-  } else {
-    //store and show user action
-    image.icon_name = "cross-large-symbolic";
-    //calculate pc reaction
-    let pc_is_thinking = true;
-    while (pc_is_thinking) {
-      let pc_is_thinking_row = "" + Math.floor(Math.random() * 3);
-      let pc_is_thinking_col = "" + Math.floor(Math.random() * 3);
-      //make pc reaction if possible
-      let temp = workbench.builder.get_object(
-        "button" + pc_is_thinking_row + pc_is_thinking_col,
-      );
-      let temp_image = temp.get_child();
-      if (!temp_image.icon_name) {
-        //store and show pc reaction
-        temp_image.icon_name = "circle-outline-thick-symbolic";
-        pc_is_thinking = false;
-        step += 2;
-      }
-      if (step >= 8) pc_is_thinking = false;
+  if (image.icon_name) return;
+  //store and show user action
+  image.icon_name = "cross-large-symbolic";
+  //calculate pc reaction
+  let pc_is_thinking = true;
+  while (pc_is_thinking) {
+    let pc_is_thinking_row = "" + Math.floor(Math.random() * 3);
+    let pc_is_thinking_col = "" + Math.floor(Math.random() * 3);
+    //make pc reaction if possible
+    let temp = workbench.builder.get_object(
+      "button" + pc_is_thinking_row + pc_is_thinking_col,
+    );
+    let temp_image = temp.get_child();
+    if (!temp_image.icon_name) {
+      //store and show pc reaction
+      temp_image.icon_name = "circle-outline-thick-symbolic";
+      pc_is_thinking = false;
+      step += 2;
     }
+    if (step >= 8) pc_is_thinking = false;
   }
 }
-
