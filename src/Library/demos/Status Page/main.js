dd79a7812ff0d28607c24c0d6dad5a145ac19580
Datagirl <datagirlcmr@gmail.com>
2023-04-04 12:40:36 +0100

    made recommended fixes

--- src/Library/demos/Status Page/main.js
+++ src/Library/demos/Status Page/main.js
@@ -5,11 +5,10 @@ const button = workbench.builder.get_object("submit_button");
 button.connect("clicked", () => {
   const text = entry.get_text();
   if (text === "Error") {
-    content.title = "Error 404";
-  } else if (text === "No Results") {
-    content.title = content.title;
+    content.title = "Error connecting to server";
+  } else if (text === "No results") {
+    content.title = "No results found";
   } else {
     content.title = "No content available for this keyword.";
   }
 });
-
