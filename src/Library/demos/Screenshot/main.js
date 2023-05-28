fd8dd03136fdede84cfe6c2a377cbfc03547842d
Sriyansh Shivam <sriyanshshivam1@gmail.com>
2023-05-28 19:54:45 +0530

    Updated code and improved UI

--- src/Library/demos/Screenshot/main.js
+++ src/Library/demos/Screenshot/main.js
@@ -1,13 +1,14 @@
 import Xdp from "gi://Xdp";
 import XdpGtk from "gi://XdpGtk4";
-const { GdkPixbuf, Gio, GLib, GObject, Gst, Gtk, Pango, PangoCairo } =
-  imports.gi;
+import Gio from "gi://Gio";
+import GLib from "gi://GLib";
+import Gtk from "gi://Gtk";
 
-const Screenshot = workbench.builder.get_object("Screenshot");
+const button = workbench.builder.get_object("button");
 const portal = new Xdp.Portal();
 const parent = XdpGtk.parent_new_gtk(workbench.window);
-
-function _takeScreenshot() {
+const picture = workbench.builder.get_object("picture");
+function takeScreenshot() {
   let flags = Xdp.ScreenshotFlags.NONE;
 
   portal.take_screenshot(parent, flags, null, (portal, result) => {
@@ -15,14 +16,10 @@ function _takeScreenshot() {
 
     try {
       const uri = portal.take_screenshot_finish(result);
-      const [path] = GLib.filename_from_uri(uri);
-      const pixbuf = GdkPixbuf.Pixbuf.new_from_file_at_scale(
-        path,
-        60,
-        40,
-        true,
-      );
-      workbench.builder.get_object("picture").set_pixbuf(pixbuf);
+      const file = Gio.File.new_for_uri(uri);
+      picture.width_request = 180;
+      picture.height_request = 180;
+      picture.set_file(file);
     } catch (e) {
       logError(e);
       return;
@@ -30,5 +27,7 @@ function _takeScreenshot() {
   });
 }
 
-Screenshot.connect("clicked", _takeScreenshot);
-
+button.connect("clicked", () => {
+  takeScreenshot();
+  console.log("Screenshot Captured");
+});
