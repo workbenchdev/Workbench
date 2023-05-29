b92d7cddfa00da0c4779a5a517cdee24527e63c2
Sriyansh Shivam <sriyanshshivam1@gmail.com>
2023-05-29 21:31:57 +0530

    Updated JS Code

--- src/Library/demos/Screenshot/main.js
+++ src/Library/demos/Screenshot/main.js
@@ -8,26 +8,34 @@ const button = workbench.builder.get_object("button");
 const portal = new Xdp.Portal();
 const parent = XdpGtk.parent_new_gtk(workbench.window);
 const picture = workbench.builder.get_object("picture");
-function takeScreenshot() {
-  let flags = Xdp.ScreenshotFlags.NONE;
 
-  portal.take_screenshot(parent, flags, null, (portal, result) => {
-    let path = null;
-
-    try {
-      const uri = portal.take_screenshot_finish(result);
-      const file = Gio.File.new_for_uri(uri);
-      picture.width_request = 180;
-      picture.height_request = 180;
-      picture.set_file(file);
-    } catch (e) {
-      logError(e);
-      return;
-    }
+function takeScreenshotAsync(parent, flags) {
+  return new Promise((resolve, reject) => {
+    portal.take_screenshot(parent, flags, null, (portal, result) => {
+      try {
+        const uri = portal.take_screenshot_finish(result);
+        const file = Gio.File.new_for_uri(uri);
+        resolve(file);
+      } catch (error) {
+        console.log(error);
+      }
+    });
   });
 }
 
-button.connect("clicked", () => {
-  takeScreenshot();
+async function fitPicture() {
+  const flags = Xdp.ScreenshotFlags.NONE;
+  try {
+    const file = await takeScreenshotAsync(parent, flags);
+    picture.width_request = 180;
+    picture.height_request = 180;
+    picture.set_file(file);
+  } catch (error) {
+    console.error("Error capturing screenshot:", error);
+  }
+}
+
+button.connect("clicked", async () => {
+  await fitPicture();
   console.log("Screenshot Captured");
 });
