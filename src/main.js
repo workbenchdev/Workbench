402773321e4ef8fe9caeab0ef5e47cf83c9f3679
Sonny Piers <sonny@fastmail.net>
2023-05-29 10:38:13 +0200

    Remove unecessary gresource

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import application from "./application.js";
 
 pkg.initGettext();
 
+import "./language-specs/blueprint.lang";
+
 export function main(argv) {
   return application.runAsync(argv);
 }
