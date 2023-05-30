f8b617d0db697ddc8a535ca0f3bac436504fb00a
halfmexican <wlhunter@go.olemiss.edu>
2023-05-29 22:01:50 -0500

    Use activatable widget property

--- src/Library/demos/Preferences Window/main.js
+++ src/Library/demos/Preferences Window/main.js
@@ -2,17 +2,16 @@ import Adw from "gi://Adw";
 import Gtk from "gi://Gtk";
 
 const pref_window = workbench.builder.get_object("pref_window");
-const cs_switch = workbench.builder.get_object("cs_switch");
+const dm_switch = workbench.builder.get_object("dm_switch");
+const fh_switch = workbench.builder.get_object("fh_switch");
 const appearance_page = workbench.builder.get_object("appearance_page");
 const style_manager = Adw.StyleManager.get_default();
 
 pref_window.set_visible_page(appearance_page);
 
-print(style_manager.color_scheme);
+dm_switch.active = style_manager.dark;
 
-cs_switch.active = style_manager.dark;
-
-cs_switch.connect("state-set", (widget, state) => {
+dm_switch.connect("state-set", (widget, state) => {
   // When the Switch is toggled, set the color scheme
   if (state) {
     style_manager.color_scheme = Adw.ColorScheme.FORCE_DARK;
@@ -20,4 +19,3 @@ cs_switch.connect("state-set", (widget, state) => {
     style_manager.color_scheme = Adw.ColorScheme.FORCE_LIGHT;
   }
 });
-
