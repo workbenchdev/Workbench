62e79b265d787ed126211637af02ec26d038ab68
halfmexican <halfmexicanhalfamazing@gmail.com>
2023-05-30 10:58:25 -0500

    update js

--- src/Library/demos/Preferences Window/main.js
+++ src/Library/demos/Preferences Window/main.js
@@ -3,11 +3,11 @@ import Gtk from "gi://Gtk";
 
 const pref_window = workbench.builder.get_object("pref_window");
 const dm_switch = workbench.builder.get_object("dm_switch");
-const fh_switch = workbench.builder.get_object("fh_switch");
-const appearance_page = workbench.builder.get_object("appearance_page");
+const subpage = workbench.builder.get_object("subpage");
+const subpage_row = workbench.builder.get_object("subpage_row");
+const subpage_button = workbench.builder.get_object("subpage_button");
+const toast_button = workbench.builder.get_object("toast_button");
 const style_manager = Adw.StyleManager.get_default();
-
-pref_window.set_visible_page(appearance_page);
 
 dm_switch.active = style_manager.dark;
 
@@ -18,4 +18,21 @@ dm_switch.connect("state-set", (widget, state) => {
   } else {
     style_manager.color_scheme = Adw.ColorScheme.FORCE_LIGHT;
   }
+});
+
+//Preferences windows can display subpages
+subpage_row.connect("activated", () => {
+  pref_window.present_subpage(subpage);
+});
+
+subpage_button.connect("clicked", () => {
+  pref_window.close_subpage();
+});
+
+toast_button.connect("clicked", () => {
+  const toast = new Adw.Toast({
+    title: "Preferences windows can display toasts",
+  });
+
+  pref_window.add_toast(toast);
 });
