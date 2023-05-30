88a6568d3874c9131cd784a9786116a7c49f73c5
Sriyansh Shivam <sriyanshshivam1@gmail.com>
2023-05-30 05:46:34 +0530

    Improved JS Code and applied changes

--- src/Library/demos/Screenshot/main.js
+++ src/Library/demos/Screenshot/main.js
@@ -3,39 +3,49 @@ import XdpGtk from "gi://XdpGtk4";
 import Gio from "gi://Gio";
 import GLib from "gi://GLib";
 import Gtk from "gi://Gtk";
+import Adw from "gi://Adw";
 
 const button = workbench.builder.get_object("button");
 const portal = new Xdp.Portal();
 const parent = XdpGtk.parent_new_gtk(workbench.window);
 const picture = workbench.builder.get_object("picture");
+const window = button.get_ancestor(Gtk.Window);
 
-function takeScreenshotAsync(parent, flags) {
-  return new Promise((resolve, reject) => {
-    portal.take_screenshot(parent, flags, null, (portal, result) => {
-      try {
-        const uri = portal.take_screenshot_finish(result);
-        const file = Gio.File.new_for_uri(uri);
-        resolve(file);
-      } catch (error) {
-        console.log(error);
-      }
-    });
+Gio._promisify(
+  Xdp.Portal.prototype,
+  "take_screenshot",
+  "take_screenshot_finish",
+);
+
+function createErrorDialog() {
+  const dialog = new Adw.MessageDialog({
+    heading: "Permission Error",
+    body: "Ensure Screenshot permission is enabled in Settings>Apps>Workbench",
+    close_response: "Okay",
+    modal: true,
+    transient_for: window,
   });
+
+  dialog.add_response("Okay", "Okay");
+  dialog.present();
 }
 
-async function fitPicture() {
+button.connect("clicked", () => {
   const flags = Xdp.ScreenshotFlags.NONE;
-  try {
-    const file = await takeScreenshotAsync(parent, flags);
-    picture.width_request = 180;
-    picture.height_request = 180;
-    picture.set_file(file);
-  } catch (error) {
-    console.error("Error capturing screenshot:", error);
-  }
-}
-
-button.connect("clicked", async () => {
-  await fitPicture();
-  console.log("Screenshot Captured");
+  portal.take_screenshot(parent, flags, null, (portal, result) => {
+    try {
+      const uri = portal.take_screenshot_finish(result);
+      const file = Gio.File.new_for_uri(uri);
+      picture.width_request = 180;
+      picture.height_request = 180;
+      picture.set_file(file);
+      console.log("Screenshot Captured");
+    } catch (error) {
+      if (error instanceof Gio.IOErrorEnum) {
+        createErrorDialog();
+      } else {
+        logError(error);
+      }
+    }
+  });
 });
