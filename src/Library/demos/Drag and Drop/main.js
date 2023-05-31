80d466791be24721bc8e83b5c7d5044dfd79b8b7
halfmexican <halfmexicanhalfamazing@gmail.com>
2023-05-31 16:08:47 -0500

    main.js: add requested changes

--- src/Library/demos/Drag and Drop/main.js
+++ src/Library/demos/Drag and Drop/main.js
@@ -5,54 +5,60 @@ import GObject from "gi://GObject";
 
 const list = workbench.builder.get_object("list");
 
-// Calculate the length of our ListBox
-let last_row = list.get_last_child();
-let list_length = last_row.get_index() + 1;
-
-// Create a Dragsource and DropTarget for each row
-for (let i = 0; i < list_length; i++) {
-  let row = list.get_row_at_index(i);
+// Iterate over ListBox children
+for (const row of list) {
   row.selectable = false;
   let _drag_x;
   let _drag_y;
 
-  let allocation;
-  const drag = new Gtk.DragSource();
-  const drop = Gtk.DropTarget.new(row.constructor.$gtype, Gdk.DragAction.COPY);
+  const drag_source = new Gtk.DragSource();
+  const drop_target = Gtk.DropTarget.new(
+    row.constructor.$gtype,
+    Gdk.DragAction.MOVE,
+  );
 
-  row.add_controller(drag);
-  row.add_controller(drop);
+  row.add_controller(drag_source);
+  row.add_controller(drop_target);
 
   // Drag controller
-  drag.connect("prepare", (source, _x, _y) => {
+  drag_source.connect("prepare", (source, _x, _y) => {
     _drag_x = _x;
     _drag_y = _y;
 
-    allocation = row.get_allocation();
-    let value = new GObject.Value();
+    const value = new GObject.Value();
     value.init(Gtk.ListBoxRow);
     value.set_object(row);
+
     return Gdk.ContentProvider.new_for_value(value);
   });
 
-  drag.connect("drag-begin", (drag) => {
-    list.remove(row);
-    row.set_state_flags(Gtk.StateFlags.DROP_ACTIVE, true);
-    let drag_widget = new Gtk.ListBox();
+  drag_source.connect("drag-begin", (_drag_source, drag) => {
+    const allocation = row.get_allocation();
+    const drag_widget = new Gtk.ListBox();
+
     drag_widget.set_size_request(allocation.width, allocation.height);
     drag_widget.add_css_class("boxed-list");
 
-    let drag_row = new Adw.ActionRow({ title: row.title });
+    const drag_row = new Adw.ActionRow({ title: row.title });
+    drag_row.add_prefix(
+      new Gtk.Image({ icon_name: "list-drag-handle-symbolic" }),
+    );
 
     drag_widget.append(drag_row);
     drag_widget.drag_highlight_row(drag_row);
 
-    let icon = Gtk.DragIcon.get_for_drag(drag.get_drag());
+    const icon = Gtk.DragIcon.get_for_drag(drag);
     icon.child = drag_widget;
+
+    drag.set_hotspot(_drag_x, _drag_y);
   });
 
   // Drop controller
-  drop.connect("drop", (drop, value, _x, _y) => {});
+  drop_target.connect("drop", (drop, value, _x, _y) => {
+    const value_row = value.get_object();
+    const target_index = list.get_row_at_y(_y).get_index() - 1;
 
-  drop.connect("enter", (_x, _y) => {});
+    list.remove(value_row);
+    list.insert(value_row, target_index);
+  });
 }
