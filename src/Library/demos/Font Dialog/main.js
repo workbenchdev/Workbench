1030944a6be1e4dd51b44869a9af7ef2ba49acd6
Sriyansh Shivam <sriyanshshivam1@gmail.com>
2023-06-01 09:54:52 +0530

    Improved Code

--- src/Library/demos/Font Dialog/main.js
+++ src/Library/demos/Font Dialog/main.js
@@ -1,44 +1,52 @@
 import Gtk from "gi://Gtk";
 import Gdk from "gi://Gdk";
 import Gio from "gi://Gio";
+import Pango from "gi://Pango?version=1.0";
 
-Gio._promisify(
+Gio._promisify (
   Gtk.FontDialog.prototype,
   "choose_font_and_features",
   "choose_font_and_features_finish",
-);
+  );
 
-let font_dialog_button = workbench.builder.get_object("font_dialog_button");
-let custom_button = workbench.builder.get_object("custom_button");
+Gio._promisify (
+  Gtk.FontDialog.prototype,
+  "choose_family",
+  "choose_family_finish",
+  );
 
-let dialog_standard = new Gtk.FontDialog({
+const parent = workbench.window;
+const font_dialog_button = workbench.builder.get_object("font_dialog_button");
+const custom_button = workbench.builder.get_object("custom_button");
+
+const dialog_standard = new Gtk.FontDialog ({
   title: "Select a font",
   modal: true,
-});
+  });
 font_dialog_button.set_dialog(dialog_standard);
 
 font_dialog_button.connect("notify::font-desc", () => {
-  let fontname = font_dialog_button.get_font_desc().to_string();
+  const fontname = font_dialog_button.get_font_desc().to_string();
   console.log(`Font Dialog Button: The font selected is ${fontname}`);
-});
-
-const dialog_custom = new Gtk.FontChooserDialog({
-  title: "Select a font",
-  modal: true,
-  transient_for: workbench.window,
-});
-
-let signalId = 0;
-custom_button.connect("clicked", () => {
-  dialog_custom.show();
-
-  signalId = dialog_custom.connect("response", (dialog, response_id) => {
-    if (response_id == Gtk.ResponseType.OK) {
-      const font_desc = dialog_custom.get_font_desc();
-      const font_name = font_desc.to_string();
-      console.log(`Custom Button: The font selected is ${font_name}`);
-      dialog_custom.hide();
-      dialog_custom.disconnect(signalId);
-    }
   });
-});
+
+const dialog_custom = new Gtk.FontDialog({
+  title: "Select a font-family",
+  modal: true,
+  });
+
+async function getFamily() {
+    let result;
+    try {
+      result = await dialog_custom.choose_family(parent, null, null);
+      // Assign the chosen family to the variable
+    } catch (err) {
+      logError(err);
+      return;
+    }
+    console.log(result);
+  }
+
+custom_button.connect("clicked", () => {
+  getFamily();
+  });
