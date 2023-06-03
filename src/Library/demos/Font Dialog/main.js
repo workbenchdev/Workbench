23210020aff341dea2dab031390a294cd0153148
Sonny Piers <sonny@fastmail.net>
2023-06-03 12:45:28 +0200

    consistent

--- src/Library/demos/Font Dialog/main.js
+++ src/Library/demos/Font Dialog/main.js
@@ -1,7 +1,5 @@
 import Gtk from "gi://Gtk";
-import Gdk from "gi://Gdk";
 import Gio from "gi://Gio";
-import Pango from "gi://Pango?version=1.0";
 
 Gio._promisify(
   Gtk.FontDialog.prototype,
@@ -15,37 +13,34 @@ Gio._promisify(
   "choose_family_finish",
 );
 
-const parent = workbench.window;
 const font_dialog_button = workbench.builder.get_object("font_dialog_button");
 const custom_button = workbench.builder.get_object("custom_button");
 
 const dialog_standard = new Gtk.FontDialog({
-  title: "Select a font",
+  title: "Select a Font",
   modal: true,
 });
 font_dialog_button.set_dialog(dialog_standard);
 
 font_dialog_button.connect("notify::font-desc", () => {
-  const fontname = font_dialog_button.get_font_desc().to_string();
-  console.log(`Font Dialog Button: The font selected is ${fontname}`);
+  const font_name = font_dialog_button.get_font_desc().to_string();
+  console.log(`Font: ${font_name}`);
 });
 
 const dialog_custom = new Gtk.FontDialog({
-  title: "Select a font-family",
+  title: "Select a Font Family",
   modal: true,
 });
 
-async function getFamily() {
+async function onClicked() {
   let result;
   try {
-    result = await dialog_custom.choose_family(parent, null, null);
+    result = await dialog_custom.choose_family(workbench.window, null, null);
   } catch (err) {
     logError(err);
     return;
   }
-  console.log(`The selected Font Family is: ${result.get_name()}`);
+  console.log(`Font Family: ${result.get_name()}`);
 }
 
-custom_button.connect("clicked", () => {
-  getFamily();
-});
+custom_button.connect("clicked", onClicked);
