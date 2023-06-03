947129b1a29f4a7e3b257999a3100de9d4e36ffa
Sriyansh Shivam <96797205+SoNiC-HeRE@users.noreply.github.com>
2023-06-03 17:20:26 +0530

    library: Add Font Dialog Entry (#244)

---
+++ src/Library/demos/Font Dialog/main.js
@@ -0,0 +1,46 @@
+import Gtk from "gi://Gtk";
+import Gio from "gi://Gio";
+
+Gio._promisify(
+  Gtk.FontDialog.prototype,
+  "choose_font_and_features",
+  "choose_font_and_features_finish",
+);
+
+Gio._promisify(
+  Gtk.FontDialog.prototype,
+  "choose_family",
+  "choose_family_finish",
+);
+
+const font_dialog_button = workbench.builder.get_object("font_dialog_button");
+const custom_button = workbench.builder.get_object("custom_button");
+
+const dialog_standard = new Gtk.FontDialog({
+  title: "Select a Font",
+  modal: true,
+});
+font_dialog_button.set_dialog(dialog_standard);
+
+font_dialog_button.connect("notify::font-desc", () => {
+  const font_name = font_dialog_button.get_font_desc().to_string();
+  console.log(`Font: ${font_name}`);
+});
+
+const dialog_custom = new Gtk.FontDialog({
+  title: "Select a Font Family",
+  modal: true,
+});
+
+async function onClicked() {
+  let result;
+  try {
+    result = await dialog_custom.choose_family(workbench.window, null, null);
+  } catch (err) {
+    logError(err);
+    return;
+  }
+  console.log(`Font Family: ${result.get_name()}`);
+}
+
+custom_button.connect("clicked", onClicked);
