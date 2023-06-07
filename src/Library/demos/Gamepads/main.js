9c2472baf7fc9ff5a3d813e8e8c738bd62df59c0
halfmexican <halfmexicanhalfamazing@gmail.com>
2023-06-07 13:06:50 -0500

    add note and button

--- src/Library/demos/Gamepads/main.js
+++ src/Library/demos/Gamepads/main.js
@@ -1,21 +1,18 @@
+/*To use USB devices in your app you will need to set the --device=all permission.
+ Workbench doesn't have such permission by default so to run this example you have to  */
 import Gtk from "gi://Gtk";
 import Manette from "gi://Manette";
 
+const continue_button = workbench.builder.get_object("continue_button");
 const monitor = Manette.Monitor.new();
-
-monitor.connect("device-connected", (monitor, device) => {
-  console.log(`Device added: ${device.get_name()}`);
-});
-
-monitor.connect("device-disconnected", (monitor, device) => {
-  console.log(`Device removed: ${device.get_name()}`);
-});
-
-const monitorIter = monitor.iterate();
+const monitor_iter = monitor.iterate();
 
 // Iterate over the devices and log their details
-let [has_next, device] = monitorIter.next();
+let [has_next, device] = monitor_iter.next();
 while (device !== null) {
-  console.log("Device:", device);
-  [has_next, device] = monitorIter.next();
+  console.log("Device:", device.get_name());
+  [has_next, device] = monitor_iter.next();
+
+  continue_button.sensitive = true;
+  continue_button.add_css_class("suggested-action");
 }
