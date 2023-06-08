7d7b70493317316d493705f0fc3ca15b63c7d9e7
halfmexican <halfmexicanhalfamazing@gmail.com>
2023-06-08 13:59:58 -0500

    add facebutton and d-pad support

--- src/Library/demos/Gamepads/main.js
+++ src/Library/demos/Gamepads/main.js
@@ -1,18 +1,39 @@
 /*To use USB devices in your app you will need to set the --device=all permission.
- Workbench doesn't have such permission by default so to run this example you have to  */
+ Workbench doesn't have such permission by default so to run this example you have to run the command
+ flatpak override re.sonny.Workbench --device=all */
+
 import Gtk from "gi://Gtk";
 import Manette from "gi://Manette";
 
-const continue_button = workbench.builder.get_object("continue_button");
+const status_page = workbench.builder.get_object("status_page");
+
 const monitor = Manette.Monitor.new();
 const monitor_iter = monitor.iterate();
 
 // Iterate over the devices and log their details
 let [has_next, device] = monitor_iter.next();
+
 while (device !== null) {
   console.log("Device:", device.get_name());
-  [has_next, device] = monitor_iter.next();
 
-  continue_button.sensitive = true;
-  continue_button.add_css_class("suggested-action");
+  status_page.title = _("Controller connected");
+  status_page.description = "";
+
+  // Face and Shoulder Buttons
+  device.connect("button-press-event", (device, event) => {
+    console.log(
+      `Device: ${device.get_name()} pressed ${event.get_hardware_code()}`,
+    );
+  });
+
+  // D-pads
+  device.connect("hat-axis-event", (device, event) => {
+    let [, hat_axis, hat_value] = event.get_hat();
+
+    console.log(
+      `Device: ${device.get_name()} moved axis ${hat_axis} to ${hat_value}`,
+    );
+  });
+
+  [has_next, device] = monitor_iter.next();
 }
