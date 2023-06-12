3d5f7597d805fdbfa634da37650ca4492542e996
halfmexican <halfmexicanhalfamazing@gmail.com>
2023-06-11 19:43:41 -0500

    use correct signal

--- src/Library/demos/Drop Down/main.js
+++ src/Library/demos/Drop Down/main.js
@@ -5,8 +5,7 @@ import GObject from "gi://GObject";
 
 const drop_down = workbench.builder.get_object("drop_down");
 
-drop_down.connect("notify", () => {
+drop_down.connect("notify::selected-item", () => {
   const selected_item = drop_down.selected_item.get_string();
-
   console.log(selected_item);
 });
