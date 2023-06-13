4c06335b2349bd43a83c2ef4284074b992b56387
AkshayWarrier <aksmen121@gmail.com>
2023-06-13 21:33:15 +0530

    library: Add Revealer entry

---
+++ src/Library/demos/Revealer/main.js
@@ -0,0 +1,13 @@
+const button = workbench.builder.get_object("button");
+const revealer = workbench.builder.get_object("revealer");
+button.connect("clicked", () => {
+  revealer.reveal_child = !revealer.reveal_child;
+  switch (button.label) {
+    case "Show":
+      button.label = "Hide";
+      return;
+    case "Hide":
+      button.label = "Show";
+      return;
+  }
+});
