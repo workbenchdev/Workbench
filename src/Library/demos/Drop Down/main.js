94253275c307f283405220ec6b1432b0be0c2c0b
halfmexican <halfmexicanhalfamazing@gmail.com>
2023-06-13 17:00:38 -0500

    add advanced dropdown with search

--- src/Library/demos/Drop Down/main.js
+++ src/Library/demos/Drop Down/main.js
@@ -1,6 +1,24 @@
+const Gtk = imports.gi.Gtk;
+const GObject = imports.gi.GObject;
+const { Gio, GLib } = imports.gi;
+
 const drop_down = workbench.builder.get_object("drop_down");
+const advanced_drop_down = workbench.builder.get_object("advanced_drop_down");
 
 drop_down.connect("notify::selected-item", () => {
   const selected_item = drop_down.selected_item.get_string();
+  console.log(selected_item);
+});
+
+const expression = new Gtk.ClosureExpression(
+  GObject.TYPE_STRING,
+  (obj) => obj.string,
+  null,
+);
+
+advanced_drop_down.expression = expression;
+
+advanced_drop_down.connect("notify::selected-item", () => {
+  const selected_item = advanced_drop_down.selected_item.get_string();
   console.log(selected_item);
 });
