8d9971e0f033834f8e9f136649c31382048da3fd
Sriyansh Shivam <sriyanshshivam1@gmail.com>
2023-06-14 17:24:19 +0530

    Refined Code

--- src/Library/demos/Email/main.js
+++ src/Library/demos/Email/main.js
@@ -1,32 +1,36 @@
 import Xdp from "gi://Xdp";
 import XdpGtk from "gi://XdpGtk4";
 import Gio from "gi://Gio";
+import Adw from "gi://Adw";
 
 const portal = new Xdp.Portal();
 const parent = XdpGtk.parent_new_gtk(workbench.window);
 
 const button = workbench.builder.get_object("button");
+const picture = workbench.builder.get_object("picture");
 
-function handleClick() {
-  portal.compose_email(
-    parent,
-    ["sonicworks05@gmail.com"],
-    ["sriyanshshivam1@gmail.com"],
-    null,
-    "Demo Message",
-    "Demo Content",
-    null,
-    Xdp.EmailFlags.NONE,
-    null,
-    (parent, result) => {
-      try {
-        portal.compose_email_finish(result);
-        log("Email sent");
-      } catch (e) {
-        logError(e);
-      }
-    },
-  );
+Gio._promisify(Xdp.Portal.prototype, "compose_email", "compose_email_finish");
+
+async function sendEmail() {
+  try {
+    const result = await portal.compose_email(
+      parent,
+      ["sonicworks05@gmail.com"],
+      ["test@gmail.com"],
+      null,
+      "Demo Message",
+      "Demo Content",
+      null,
+      Xdp.EmailFlags.NONE,
+      null,
+    );
+
+    if (result) {
+      console.log("Email app opened");
+    }
+  } catch (err) {
+    logError(err);
+  }
 }
 
-button.connect("clicked", handleClick);
+button.connect("clicked", sendEmail);
