0ee1101ef7d9978c47e7b6b08446ccbe0f4adf75
AkshayWarrier <aksmen121@gmail.com>
2023-06-14 13:09:21 +0530

    Revealer: Minor change

--- src/Library/demos/Revealer/main.js
+++ src/Library/demos/Revealer/main.js
@@ -1,5 +1,6 @@
 const button = workbench.builder.get_object("button");
 const revealer = workbench.builder.get_object("revealer");
+
 button.connect("clicked", () => {
   revealer.reveal_child = !revealer.reveal_child;
   switch (button.label) {
