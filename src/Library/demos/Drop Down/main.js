26804b89a15fd1e6126d11e5f283f0a678220bd2
halfmexican <halfmexicanhalfamazing@gmail.com>
2023-06-15 14:47:52 -0500

    main.js: remove searchable_dropdown

--- src/Library/demos/Drop Down/main.js
+++ src/Library/demos/Drop Down/main.js
@@ -3,24 +3,10 @@ import GObject from "gi://GObject";
 import Gio from "gi://Gio";
 
 const drop_down = workbench.builder.get_object("drop_down");
-const searchable_drop_down = workbench.builder.get_object("searchable_drop_down");
 const advanced_drop_down = workbench.builder.get_object("advanced_drop_down");
 
 drop_down.connect("notify::selected-item", () => {
   const selected_item = drop_down.selected_item.get_string();
-  console.log(selected_item);
-});
-
-const search_expression = new Gtk.ClosureExpression(
-  GObject.TYPE_STRING,
-  (obj) => obj.string,
-  null,
-);
-
-searchable_drop_down.expression = search_expression;
-
-searchable_drop_down.connect("notify::selected-item", () => {
-  const selected_item = searchable_drop_down.selected_item.get_string();
   console.log(selected_item);
 });
 
@@ -48,17 +34,30 @@ const key_value_pair = GObject.registerClass(
 
 const model = new Gio.ListStore();
 
-model.append(new key_value_pair({ key: "one", value: "One" }));
-model.append(new key_value_pair({ key: "two", value: "Two" }));
-model.append(new key_value_pair({ key: "three", value: "Three" }));
+model.append(new key_value_pair({ key: "win7", value: "Windows 7" }));
+model.append(new key_value_pair({ key: "win10", value: "Windows 10" }));
+model.append(new key_value_pair({ key: "win11", value: "Windows 11" }));
+model.append(new key_value_pair({ key: "ubuntu", value: "Ubuntu" }));
+model.append(new key_value_pair({ key: "fedora", value: "Fedora" }));
+model.append(new key_value_pair({ key: "debian", value: "Debian" }));
+model.append(new key_value_pair({ key: "mint", value: "Mint" }));
+model.append(new key_value_pair({ key: "arch", value: "Arch Linux" }));
+model.append(new key_value_pair({ key: "popos", value: "Pop!_OS" }));
+model.append(new key_value_pair({ key: "opensuse", value: "OpenSUSE" }));
+model.append(new key_value_pair({ key: "gentoo", value: "Gentoo" }));
+model.append(new key_value_pair({ key: "freebsd", value: "FreeBSD" }));
+model.append(new key_value_pair({ key: "macos", value: "macOS" }));
+model.append(new key_value_pair({ key: "ios", value: "iOS" }));
+model.append(new key_value_pair({ key: "android", value: "Android" }));
 
-const custom_expression = Gtk.PropertyExpression.new(
+
+const expression = Gtk.PropertyExpression.new(
   key_value_pair.$gtype,
   null,
   "value",
 );
 
-advanced_drop_down.expression = custom_expression;
+advanced_drop_down.expression = expression;
 advanced_drop_down.model = model;
 
 advanced_drop_down.connect("notify::selected-item", () => {
