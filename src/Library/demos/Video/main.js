49343712049c5faa0e01cd8282ccce04167950ec
halfmexican <halfmexicanhalfamazing@gmail.com>
2023-06-15 18:13:08 -0500

    add Gtk.Video demo

---
+++ src/Library/demos/Video/main.js
@@ -0,0 +1,29 @@
+import Adw from "gi://Adw";
+import Gtk from "gi://Gtk";
+
+const page = workbench.builder.get_object("page");
+const video = workbench.builder.get_object("video");
+const file_button = workbench.builder.get_object("file_button");
+let file;
+video.set_autoplay(true);
+
+file_button.connect("clicked", () => {
+  const dialog = new Gtk.FileDialog({
+    title: "Select a Video File",
+    modal: true,
+  });
+
+  dialog.open(page.get_root(), null, fileDialogCallback);
+});
+
+function fileDialogCallback(dialog, res) {
+  try {
+    file = dialog.open_finish(res);
+    if (file) {
+      console.log(`Selected file: ${file.get_path()}`);
+      video.file = file;
+    }
+  } catch (e) {
+    console.logError(e);
+  }
+}
