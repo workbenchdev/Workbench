b049ee961c02cd93c2e2165eca8edc74eb072f8d
Sriyansh Shivam <sriyanshshivam1@gmail.com>
2023-06-16 13:13:59 +0530

    Applied suggested Changes

--- src/Library/demos/Wallpaper/main.js
+++ src/Library/demos/Wallpaper/main.js
@@ -1,5 +1,3 @@
-import Gtk from "gi://Gtk";
-import Gdk from "gi://Gdk";
 import Gio from "gi://Gio";
 import Xdp from "gi://Xdp";
 import XdpGtk from "gi://XdpGtk4";
@@ -7,35 +5,24 @@ import XdpGtk from "gi://XdpGtk4";
 const portal = new Xdp.Portal();
 const parent = XdpGtk.parent_new_gtk(workbench.window);
 const button = workbench.builder.get_object("button");
+const file = Gio.File.new_for_path(pkg.pkgdatadir).resolve_relative_path(
+  "Library/demos/Wallpaper/image.jpg",
+);
 
-Gio._promisify(Gtk.FileDialog.prototype, "open", "open_finish");
 Gio._promisify(Xdp.Portal.prototype, "set_wallpaper", "set_wallpaper_finish");
 
-const filter = new Gtk.FileFilter({
-  name: "File",
-});
-
-const dialog = new Gtk.FileDialog({
-  title: "Select a File",
-  modal: true,
-  default_filter: filter,
-});
-
-async function setWallpaper() {
+async function onClicked() {
   const flags = Xdp.WallpaperFlags.PREVIEW;
-  try {
-    const file = await dialog.open(workbench.window, null);
-    const path = file.get_path();
-    const uri = "file:///" + path;
-    const result = await portal.set_wallpaper(parent, uri, flags, null);
+  const uri = file.get_uri();
+  const success = await portal.set_wallpaper(parent, uri, flags, null);
 
-    if (result) {
-      console.log("Wallpaper set successfully");
-    }
-  } catch (err) {
-    logError(err);
-    return;
+  if (success) {
+    console.log("Wallpaper set successfully");
+  } else {
+    console.log("Could not set wallpaper");
   }
 }
 
-button.connect("clicked", setWallpaper);
+button.connect("clicked", () => {
+  onClicked().catch(logError);
+});
