01afa9109632a6ec1d799a83350abb2b166ff7f3
Sriyansh Shivam <sriyanshshivam1@gmail.com>
2023-06-16 12:53:22 +0530

    Applied Suggested Changes

--- src/Library/demos/Color Picker/main.js
+++ src/Library/demos/Color Picker/main.js
@@ -1,7 +1,5 @@
-import Gtk from "gi://Gtk";
 import Gdk from "gi://Gdk";
 import Gio from "gi://Gio";
-import GLib from "gi://GLib";
 import Xdp from "gi://Xdp";
 import XdpGtk from "gi://XdpGtk4";
 
@@ -11,16 +9,16 @@ const button = workbench.builder.get_object("button");
 
 Gio._promisify(Xdp.Portal.prototype, "pick_color", "pick_color_finish");
 
-async function pickColor() {
-  try {
-    const color = await portal.pick_color(parent, null);
-    const colorInfo = color.print(false);
+async function onClicked() {
+  // result is a GVariant of the form (ddd), containing red, green and blue components in the range [0,1]
+  const result = await portal.pick_color(parent, null);
+  const [r, g, b] = result.deepUnpack();
 
-    // The picked color is a GVariant of the form (ddd), containing red, green and blue components in the range [0,1].
-    console.log(`Selected Color is: ${colorInfo}`);
-  } catch (err) {
-    logError(err);
-  }
+  const rgbColor = new Gdk.RGBA({ red: r, green: g, blue: b, alpha: 1.0 });
+  const color = rgbColor.to_string();
+  console.log(`Selected Color is: ${color}`);
 }
 
-button.connect("clicked", pickColor);
+button.connect("clicked", () => {
+  onClicked().catch(logError);
+});
