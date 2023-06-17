a3966fbab420ef092b9d60dada7e211750842147
Sonny Piers <sonny@fastmail.net>
2023-06-17 19:18:02 +0200

    Minor changes

--- src/Library/demos/Email/main.js
+++ src/Library/demos/Email/main.js
@@ -2,43 +2,33 @@ import Xdp from "gi://Xdp";
 import XdpGtk from "gi://XdpGtk4";
 import Gio from "gi://Gio";
 
+Gio._promisify(Xdp.Portal.prototype, "compose_email", "compose_email_finish");
+
 const portal = new Xdp.Portal();
 const parent = XdpGtk.parent_new_gtk(workbench.window);
 
 const button = workbench.builder.get_object("button");
 const entry = workbench.builder.get_object("entry");
 
-Gio._promisify(Xdp.Portal.prototype, "compose_email", "compose_email_finish");
-
-//E-Mail Validator
-function isValidEmail(email) {
-  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  return emailRegex.test(email);
-}
-
 async function onClicked() {
-  const email = entry.get_text();
-  if (isValidEmail(email)) {
-    const success = await portal.compose_email(
-      parent,
-      [email], //address
-      null, //cc
-      null, //bcc
-      "Email from Workbench", //subject
-      "Hello World!", //message
-      null, //attachments
-      Xdp.EmailFlags.NONE,
-      null,
-    );
+  const email_address = entry.get_text();
 
-    if (success) {
-      console.log("Success");
-    } else {
-      console.log("Failure, verify that you have a email application.");
-    }
+  const success = await portal.compose_email(
+    parent,
+    [email_address], // addresses
+    null, // cc
+    null, // bcc
+    "Email from Workbench", // subject
+    "Hello World!", // body
+    null, // attachments
+    Xdp.EmailFlags.NONE, // flags
+    null, // cancellable
+  );
+
+  if (success) {
+    console.log("Success");
   } else {
-    console.log("Invalid email address");
-    return;
+    console.log("Failure, verify that you have an email application.");
   }
 }
 
