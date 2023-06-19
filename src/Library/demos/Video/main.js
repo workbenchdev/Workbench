3983abe1962075b353b6cc51689af7e6b33f1a17
halfmexican <103920890+halfmexican@users.noreply.github.com>
2023-06-19 14:31:11 -0500

    library: add Gtk.Video Demo (#340)

---
+++ src/Library/demos/Video/main.js
@@ -0,0 +1,21 @@
+import Gtk from "gi://Gtk";
+import Gio from "gi://Gio";
+
+const video = workbench.builder.get_object("video");
+
+video.file = Gio.File.new_for_path(pkg.pkgdatadir).resolve_relative_path(
+  "Library/demos/Video/workbench-video.mp4",
+);
+
+const click_gesture = new Gtk.GestureClick();
+
+click_gesture.connect("pressed", () => {
+  const media_stream = video.media_stream;
+  if (media_stream.playing) {
+    media_stream.pause();
+  } else {
+    media_stream.play();
+  }
+});
+
+video.add_controller(click_gesture);
