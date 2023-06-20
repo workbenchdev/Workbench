955aa31a5a43e3a75847598d6bbfc3ad0e01a0a3
Sriyansh Shivam <sriyanshshivam1@gmail.com>
2023-06-20 13:27:17 +0530

    Applied Suggested Changes

--- src/Library/demos/Account/main.js
+++ src/Library/demos/Account/main.js
@@ -1,17 +1,7 @@
 import Xdp from "gi://Xdp";
 import XdpGtk from "gi://XdpGtk4";
 import Gio from "gi://Gio";
-import Adw from "gi://Adw";
 import Gdk from "gi://Gdk";
-
-const portal = new Xdp.Portal();
-const parent = XdpGtk.parent_new_gtk(workbench.window);
-
-const button = workbench.builder.get_object("button");
-const avatar = workbench.builder.get_object("avatar");
-const entry = workbench.builder.get_object("entry");
-const username = workbench.builder.get_object("username");
-const display = workbench.builder.get_object("name");
 
 Gio._promisify(
   Xdp.Portal.prototype,
@@ -19,26 +9,39 @@ Gio._promisify(
   "get_user_information_finish",
 );
 
-async function onClicked() {
-  // call the portal function
-  const reason = entry.get_text();
-  let user_info = null;
-  user_info = await portal.get_user_information(parent, reason, null, null);
+const portal = new Xdp.Portal();
+const parent = XdpGtk.parent_new_gtk(workbench.window);
 
-  //retrieve values from GVariant
-  user_info = user_info.deepUnpack();
+const revealer = workbench.builder.get_object("revealer");
+const button = workbench.builder.get_object("button");
+const avatar = workbench.builder.get_object("avatar");
+const entry = workbench.builder.get_object("entry");
+const username = workbench.builder.get_object("username");
+const display = workbench.builder.get_object("name");
+
+async function onClicked() {
+  const reason = entry.get_text();
+  const result = await portal.get_user_information(parent, reason, null, null);
+
+  /*
+  result in the form of a GVariant dictionary containing the following fields:
+  id (s): the user ID
+  name (s): the users real name
+  image (s): the uri of an image file for the users avatar picture.
+  */
+
+  const user_info = result.deepUnpack();
   const id = user_info["id"].deepUnpack();
   const name = user_info["name"].deepUnpack();
   const uri = user_info["image"].deepUnpack();
   const file = Gio.File.new_for_uri(uri);
   const texture = Gdk.Texture.new_from_file(file);
 
-  //update values
   username.label = id;
   display.label = name;
   avatar.set_custom_image(texture);
+  revealer.reveal_child = true;
 
-  //post-request updation
   entry.set_text("");
   console.log("Information retrieved");
 }
