0f14cb43bb1a4a9a3af201081a2022be94211b89
AkshayWarrier <58233418+AkshayWarrier@users.noreply.github.com>
2023-06-20 23:36:00 +0530

    library: Add Revealer entry (#331)

---
+++ src/Library/demos/Revealer/main.js
@@ -0,0 +1,34 @@
+import Gio from "gi://Gio";
+
+const button_slide = workbench.builder.get_object("button_slide");
+const button_crossfade = workbench.builder.get_object("button_crossfade");
+const revealer_slide = workbench.builder.get_object("revealer_slide");
+const revealer_crossfade = workbench.builder.get_object("revealer_crossfade");
+const image1 = workbench.builder.get_object("image1");
+const image2 = workbench.builder.get_object("image2");
+
+let file = Gio.File.new_for_path(pkg.pkgdatadir).resolve_relative_path(
+  "Library/demos/Revealer/image1.png",
+);
+image1.file = file;
+
+file = Gio.File.new_for_path(pkg.pkgdatadir).resolve_relative_path(
+  "Library/demos/Revealer/image2.png",
+);
+image2.file = file;
+
+button_slide.connect("toggled", () => {
+  revealer_slide.reveal_child = button_slide.active;
+});
+
+button_crossfade.connect("toggled", () => {
+  revealer_crossfade.reveal_child = button_crossfade.active;
+});
+
+revealer_slide.connect("notify::child-revealed", () => {
+  if (revealer_slide.child_revealed) {
+    console.log("Slide Revealer Shown");
+  } else {
+    console.log("Slide Revealer Hidden");
+  }
+});
