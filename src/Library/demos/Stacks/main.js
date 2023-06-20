3bd70dc31df7f6bab01d8df3c391ae6f6bf9ece0
halfmexican <halfmexicanhalfamazing@gmail.com>
2023-06-20 15:47:52 -0500

    main.js: add seperator

--- src/Library/demos/Stacks/main.js
+++ src/Library/demos/Stacks/main.js
@@ -12,6 +12,7 @@ const transition_spin_button = workbench.builder.get_object(
   "transition_spin_button",
 );
 let navigation_widget;
+let separator;
 
 if (navigation_row.get_selected() === 0) {
   navigation_widget = new Gtk.StackSwitcher({ stack: stack });
@@ -24,12 +25,15 @@ if (navigation_row.get_selected() === 0) {
 navigation_row.connect("notify::selected-item", () => {
   if (navigation_row.get_selected() === 0) {
     root_box.remove(navigation_widget);
+    root_box.remove(separator);
     navigation_widget = new Gtk.StackSwitcher({ stack: stack });
     root_box.prepend(navigation_widget);
     root_box.orientation = Gtk.Orientation.VERTICAL;
   } else {
     root_box.remove(navigation_widget);
     navigation_widget = new Gtk.StackSidebar({ stack: stack });
+    separator = new Gtk.Separator();
+    root_box.prepend(separator);
     root_box.prepend(navigation_widget);
     root_box.orientation = Gtk.Orientation.HORIZONTAL;
   }
@@ -41,14 +45,3 @@ transition_row.connect("notify::selected-item", () => {
   stack.transition_type = transition_row.get_selected();
 });
 
-stack.interpolate = interpolate_switch.active;
-
-interpolate_switch.connect("notify::active", () => {
-  stack.interpolate = interpolate_switch.active;
-});
-
-stack.transition_duration = transition_spin_button.value;
-
-transition_spin_button.connect("value-changed", () => {
-  stack.transition_duration = transition_spin_button.value;
-});
