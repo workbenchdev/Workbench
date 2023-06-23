3f87f104f6e2edb01e865c4bafd8eab08df4c601
halfmexican <halfmexicanhalfamazing@gmail.com>
2023-06-22 19:19:50 -0500

    add gestureswipe example

--- src/Library/demos/Event Controllers/main.js
+++ src/Library/demos/Event Controllers/main.js
@@ -1,9 +1,25 @@
 import Gtk from "gi://Gtk";
 import Gdk from "gi://Gdk";
+import Gio from "gi://Gio";
 
 const window = workbench.window;
 const ctrl_button = workbench.builder.get_object("ctrl_button");
 const click_button = workbench.builder.get_object("click_button");
+const stack = workbench.builder.get_object("stack");
+const pic1 = workbench.builder.get_object("pic1");
+const pic2 = workbench.builder.get_object("pic2");
+
+const pic1_file = Gio.File.new_for_path(pkg.pkgdatadir).resolve_relative_path(
+  "Library/demos/Event Controllers/image1.png",
+);
+
+const pic2_file = Gio.File.new_for_path(pkg.pkgdatadir).resolve_relative_path(
+  "Library/demos/Event Controllers/image2.png",
+);
+
+pic1.file = pic1_file;
+pic2.file = pic2_file;
+
 let ctrl_pressed = false;
 let right_clicks = 0;
 
@@ -35,7 +51,6 @@ ctrl_button.connect("clicked", () => {
   }
 });
 
-
 const gesture_secondary_click = new Gtk.GestureClick({
   button: Gdk.BUTTON_SECONDARY,
 });
@@ -43,6 +58,17 @@ const gesture_secondary_click = new Gtk.GestureClick({
 click_button.add_controller(gesture_secondary_click);
 gesture_secondary_click.connect("pressed", (gesture, n_press, x, y) => {
   right_clicks += 1;
-  click_button.label = (right_clicks).toString();
-  console.log("right-click");
+  click_button.label = right_clicks.toString();
+});
+
+const single = new Gtk.GestureSwipe();
+
+stack.add_controller(single);
+
+single.connect("swipe", (controller, vel_x, vel_y) => {
+  if (vel_x > 0) {
+    stack.set_visible_child_name("pic1");
+  } else {
+    stack.set_visible_child_name("pic2");
+  }
 });
