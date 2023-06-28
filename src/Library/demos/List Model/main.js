784f20a68c957a2256bbc1ba474461730b0f475b
Sriyansh Shivam <sriyanshshivam1@gmail.com>
2023-06-28 22:35:41 +0530

    Made Changes as Discussed

--- src/Library/demos/List Model/main.js
+++ src/Library/demos/List Model/main.js
@@ -12,56 +12,75 @@ const flow_box = workbench.builder.get_object("flow_box");
 const add = workbench.builder.get_object("add");
 const remove = workbench.builder.get_object("remove");
 
+//ListModel initialization and binding
+const model = new Gtk.StringList();
+let item = 1;
+
+function createItemForListBox(listItem) {
+  const listRow = new Adw.ActionRow({
+    title: listItem.string,
+  });
+  return listRow;
+}
+
+function createItemForFlowBox(listItem) {
+  const listRow = new Adw.ActionRow({
+    title: listItem.string,
+  });
+  return listRow;
+}
+
+list_box.bind_model(model, createItemForListBox);
+flow_box.bind_model(model, createItemForFlowBox);
+
+//Handling for ListBox
+const isListBoxActive = toggle_list_box.get_active();
+reveal_list_box.reveal_child = isListBoxActive;
+if (isListBoxActive) {
+  add.connect("clicked", () => {
+    const newItem = `Item ${item}`;
+    model.append(newItem); // Append the new item as an array to the model
+    item++;
+  });
+  remove.connect("clicked", () => {
+    const length = model.get_n_items();
+    model.remove(length - 1);
+  });
+}
+
+//Handling for FlowBox
+const isFlowBoxActive = toggle_flow_box.get_active();
+reveal_flow_box.reveal_child = isFlowBoxActive;
+if (isFlowBoxActive) {
+  add.connect("clicked", () => {
+    const newItem = `Item ${item}`;
+    model.append(newItem); // Append the new item as an array to the model
+    item++;
+  });
+  remove.connect("clicked", () => {
+    const length = model.get_n_items();
+    model.remove(length - 1);
+  });
+}
+
+// Check if ListBox is Active
 toggle_list_box.connect("toggled", () => {
   const isActive = toggle_list_box.get_active();
   reveal_list_box.reveal_child = isActive;
   if (isActive) {
-    console.log("ListBox view toggled on");
+    console.log("ListBox toggled on");
   } else {
-    console.log("ListBox view toggled off");
+    console.log("ListBox toggled off");
   }
 });
 
+// Check if FlowBox is Active
 toggle_flow_box.connect("toggled", () => {
   const isActive = toggle_flow_box.get_active();
   reveal_flow_box.reveal_child = isActive;
   if (isActive) {
-    console.log("FlowBox view toggled on");
-    add.connect("clicked", addRowFlowBox);
-    remove.connect("clicked", removeRowFlowBox);
+    console.log("FlowBox toggled on");
   } else {
-    console.log("FlowBox view toggled off");
+    console.log("FlowBox toggled off");
   }
 });
-
-const model = new Gtk.StringList();
-const rows = []; // Array to store the created rows
-let item = 1;
-
-function createRow(listItem) {
-  const listRow = new Adw.ActionRow({
-    title: listItem.string,
-  });
-  rows.push(listRow); // Add the created row to the array
-  item++;
-  return listRow;
-}
-
-list_box.bind_model(model, createRow);
-
-add.connect("clicked", () => {
-  const newItem = `Item ${item}`;
-  model.append(newItem); // Append the new item as an array to the model
-});
-
-remove.connect("clicked", () => {
-  const arrayLength = rows.length;
-
-  if (arrayLength > 0) {
-    const lastElementIndex = arrayLength - 1;
-    const lastElement = rows[lastElementIndex];
-
-    model.remove(rows[lastElement]);
-  }
-});
-
