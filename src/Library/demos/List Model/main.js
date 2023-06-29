55a45fb9f281ddaad801ccc660df05d1608a8675
Sriyansh Shivam <sriyanshshivam1@gmail.com>
2023-06-29 17:41:56 +0530

    Implemented Filter-Model

--- src/Library/demos/List Model/main.js
+++ src/Library/demos/List Model/main.js
@@ -12,6 +12,7 @@ const remove = workbench.builder.get_object("remove");
 const list_box_editable = workbench.builder.get_object("list_box_editable");
 const query_for_filter = workbench.builder.get_object("query_for_filter");
 const submit = workbench.builder.get_object("submit");
+
 //Model
 const model = new Gtk.StringList();
 let item = 1;
@@ -72,6 +73,12 @@ remove.connect("clicked", () => {
   model.remove(n_items - 1);
 });
 
+// Removes all widgets if searchString does'nt exist and shows only the searchString if it matches filter;
+submit.connect("clicked", () => {
+  const searchString = query_for_filter.get_text();
+  filter.search = searchString;
+  log(filter_model);
+});
 // View
 stack.connect("notify::visible-child", () => {
   if (stack.visible_child === list_box) {
