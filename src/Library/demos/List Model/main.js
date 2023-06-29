c3ef23a83863f6ebd0f1cf31e79d31b0751124a3
Sriyansh Shivam <sriyanshshivam1@gmail.com>
2023-06-29 17:03:46 +0530

    Added new page and items-changed signal

--- src/Library/demos/List Model/main.js
+++ src/Library/demos/List Model/main.js
@@ -1,5 +1,6 @@
 import Gio from "gi://Gio";
 import Gdk from "gi://Gdk";
+import GObject from "gi://GObject";
 import Gtk from "gi://Gtk";
 import Adw from "gi://Adw";
 
@@ -8,10 +9,23 @@ const list_box = workbench.builder.get_object("list_box");
 const flow_box = workbench.builder.get_object("flow_box");
 const add = workbench.builder.get_object("add");
 const remove = workbench.builder.get_object("remove");
-
+const list_box_editable = workbench.builder.get_object("list_box_editable");
+const query_for_filter = workbench.builder.get_object("query_for_filter");
+const submit = workbench.builder.get_object("submit");
 //Model
 const model = new Gtk.StringList();
 let item = 1;
+
+//Filter-Model
+const filter = new Gtk.StringFilter({
+  ignore_case: true,
+  match_mode: Gtk.StringFilterMatchMode.SUBSTRING,
+});
+const filter_model = new Gtk.FilterListModel({
+  model: model,
+  filter: filter,
+  incremental: true,
+});
 
 function createItemForListBox(listItem) {
   const listRow = new Adw.ActionRow({
@@ -27,10 +41,27 @@ function createItemForFlowBox(listItem) {
   return listRow;
 }
 
+function createItemForFilterModel(listItem) {
+  const listRow = new Adw.ActionRow({
+    title: listItem.string,
+  });
+  return listRow;
+}
+
 list_box.bind_model(model, createItemForListBox);
 flow_box.bind_model(model, createItemForFlowBox);
+list_box_editable.bind_model(filter_model, createItemForFilterModel);
 
 // Controller
+
+model.connect("items-changed", (list, position, removed, added) => {
+  console.log(
+    `position: ${position}, Item removed? ${Boolean(
+      removed,
+    )}, Item added? ${Boolean(added)}`,
+  );
+});
+
 add.connect("clicked", () => {
   const new_item = `Item ${item}`;
   model.append(new_item);
