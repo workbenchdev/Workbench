18ee4a6892525cc694733167c9807a4ad1b4717f
Sriyansh Shivam <sriyanshshivam1@gmail.com>
2023-07-04 12:46:46 +0530

    Enhanced UI and updated Code

--- src/Library/demos/Action Bar/main.js
+++ src/Library/demos/Action Bar/main.js
@@ -6,48 +6,17 @@ import Adw from "gi://Adw";
 
 const action_bar = workbench.builder.get_object("action_bar");
 const show_content = workbench.builder.get_object("show_content");
-
-const center_widget = new Adw.EntryRow({
-  title: _("This is the center widget"),
-});
-
-const prefix_widget = new Gtk.Button({
-  icon_name: "check-round-outline-whole-symbolic",
-});
-
-const suffix_widget = new Gtk.Button({
-  icon_name: "x-circular-symbolic",
-});
-
-const box = new Gtk.Box({
-  orientation: Gtk.Orientation.VERTICAL,
-  margin_top: 12,
-  margin_bottom: 12,
-});
-
-box.append(center_widget);
-
-const buttonsBox = new Gtk.Box({
-  orientation: Gtk.Orientation.HORIZONTAL,
-  spacing: 10,
-  homogeneous: true,
-});
-
-buttonsBox.append(prefix_widget);
-buttonsBox.append(suffix_widget);
-
-box.append(buttonsBox);
-
-action_bar.set_center_widget(box);
+const start_widget = workbench.builder.get_object("start_widget");
+const end_widget = workbench.builder.get_object("end_widget");
 
 show_content.connect("clicked", () => {
   action_bar.revealed = true;
 });
 
-prefix_widget.connect("clicked", () => {
-  console.log("This is the start widget");
+start_widget.connect("clicked", () => {
+  console.log("I am start widget");
 });
 
-suffix_widget.connect("clicked", () => {
-  console.log("This is the end widget");
+end_widget.connect("clicked", () => {
+  console.log("I am end widget");
 });
