5cc64c3f57bc7b219b4bf48d29a2142fb9b9f157
Sriyansh Shivam <sriyanshshivam1@gmail.com>
2023-07-04 18:46:01 +0530

    Port CodeFind feature to a demo

---
+++ src/Library/demos/Code Find/main.js
@@ -0,0 +1,166 @@
+import Adw from "gi://Adw";
+import Gtk from "gi://Gtk";
+import Source from "gi://GtkSource";
+import Gdk from "gi://Gdk";
+
+Source.init();
+
+const previous_match = workbench.builder.get_object("previous_match");
+const next_match = workbench.builder.get_object("next_match");
+const overlay = workbench.builder.get_object("overlay");
+const revealer = workbench.builder.get_object("revealer");
+const search_entry = workbench.builder.get_object("search_entry");
+const search_bar = workbench.builder.get_object("search_bar");
+//const source_view = workbench.builder.get_object("source_view");
+const scroll_view = workbench.builder.get_object("scroll_view");
+const source_view = new Source.View({
+  monospace: true,
+  auto_indent: true,
+  highlight_current_line: true,
+  indent_on_tab: true,
+  indent_width: 2,
+  insert_spaces_instead_of_tabs: true,
+  show_line_marks: true,
+  show_line_numbers: true,
+  smart_backspace: true,
+  tab_width: 2,
+  css_classes: ["card"],
+});
+const buffer = source_view.get_buffer();
+
+const sampleCode = `
+function calculateSum(a, b) {
+  return a + b;
+}
+
+function calculateProduct(a, b) {
+  return a * b;
+}
+
+function calculatePower(base, exponent) {
+  let result = 1;
+  for (let i = 0; i < exponent; i++) {
+    result *= base;
+  }
+  return result;
+}
+
+function calculateFactorial(num) {
+  if (num === 0 || num === 1) {
+    return 1;
+  }
+  let factorial = 1;
+  for (let i = 2; i <= num; i++) {
+    factorial *= i;
+  }
+  return factorial;
+}
+
+function generateFibonacciSequence(n) {
+  const sequence = [0, 1];
+  for (let i = 2; i < n; i++) {
+    const nextNumber = sequence[i - 1] + sequence[i - 2];
+    sequence.push(nextNumber);
+  }
+  return sequence;
+}
+
+function isPrimeNumber(num) {
+  if (num <= 1) {
+    return false;
+  }
+  for (let i = 2; i <= Math.sqrt(num); i++) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function getPrimeNumbersInRange(start, end) {
+  const primes = [];
+  for (let i = start; i <= end; i++) {
+    if (isPrimeNumber(i)) {
+      primes.push(i);
+    }
+  }
+  return primes;
+}
+
+// Main program
+const num1 = 10;
+const num2 = 5;
+
+const sum = calculateSum(num1, num2);
+console.log("Sum:", sum);
+
+const product = calculateProduct(num1, num2);
+console.log("Product:", product);
+
+const power = calculatePower(num1, num2);
+console.log("Power:", power);
+
+const factorial = calculateFactorial(num1);
+console.log("Factorial:", factorial);
+
+const fibonacciSequence = generateFibonacciSequence(10);
+console.log("Fibonacci Sequence:", fibonacciSequence);
+
+const primeNumbers = getPrimeNumbersInRange(1, 100);
+console.log("Prime Numbers:", primeNumbers);
+`;
+
+buffer.set_text(sampleCode, -1);
+scroll_view.set_child(source_view);
+
+//Functions
+
+//reveal_search
+const controller_key = new Gtk.EventControllerKey();
+source_view.add_controller(controller_key);
+controller_key.connect("key-pressed", (controller, keyval, keycode, state) => {
+  if (
+    (state & Gdk.ModifierType.CONTROL_MASK && keyval === Gdk.KEY_f) ||
+    (state & Gdk.ModifierType.CONTROL_MASK && keyval === Gdk.KEY_F)
+  ) {
+    revealer.reveal_child = true;
+    search_bar.search_mode_enabled = true;
+  }
+});
+
+//handleSearch
+search_bar.connect_entry(search_entry);
+const search_settings = new Source.SearchSettings({
+  case_sensitive: false,
+});
+
+const search_context = new Source.SearchContext({
+  buffer,
+  settings: search_settings,
+  highlight: true,
+});
+
+function selectSearchOccurence(match_start, match_end) {
+  buffer.select_range(match_start, match_end);
+  source_view.scroll_mark_onscreen(buffer.get_insert());
+}
+
+search_entry.connect("search-changed", () => {
+  search_settings.search_text = search_entry.get_text();
+});
+
+previous_match.connect("clicked", () => {
+  const [, iter] = buffer.get_selection_bounds();
+  const [found, match_start, match_end] = search_context.backward(iter);
+  if (!found) return;
+  // log(iter.get_offset(), match_start.get_offset(), match_end.get_offset());
+  selectSearchOccurence(match_start, match_end);
+});
+
+next_match.connect("clicked", () => {
+  const [, , iter] = buffer.get_selection_bounds();
+  const [found, match_start, match_end] = search_context.forward(iter);
+  if (!found) return;
+  // log(iter.get_offset(), match_start.get_offset(), match_end.get_offset());
+  selectSearchOccurence(match_start, match_end);
+});
