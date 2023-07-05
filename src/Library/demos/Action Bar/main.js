4201d051c7fc824489560378c6c545c9d4706e5f
Sriyansh Shivam <sriyanshshivam1@gmail.com>
2023-07-06 02:39:57 +0530

    Applied Suggested Changes

--- src/Library/demos/Action Bar/main.js
+++ src/Library/demos/Action Bar/main.js
@@ -1,16 +1,11 @@
-import Gio from "gi://Gio";
-import Gdk from "gi://Gdk";
-import GObject from "gi://GObject";
-import Gtk from "gi://Gtk";
-import Adw from "gi://Adw";
-
 const action_bar = workbench.builder.get_object("action_bar");
-const show_content = workbench.builder.get_object("show_content");
+const button = workbench.builder.get_object("button");
 const start_widget = workbench.builder.get_object("start_widget");
 const end_widget = workbench.builder.get_object("end_widget");
 
-show_content.connect("clicked", () => {
-  action_bar.revealed = true;
+button.connect("notify::active", () => {
+  console.log(`ActionBar toggled ${button.active ? "Off" : "On"}`);
+  action_bar.revealed = !button.active;
 });
 
 start_widget.connect("clicked", () => {
