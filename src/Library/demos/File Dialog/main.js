f950ace218ea890d7b5fdf248e168775e9be88a1
Sriyansh Shivam <sriyanshshivam1@gmail.com>
2023-07-09 19:30:46 +0530

    Applied Suggested Changes

--- src/Library/demos/File Dialog/main.js
+++ src/Library/demos/File Dialog/main.js
@@ -1,6 +1,7 @@
 import Gtk from "gi://Gtk";
 import Gdk from "gi://Gdk";
 import Gio from "gi://Gio";
+import GLib from "gi://GLib";
 
 Gio._promisify(Gtk.FileDialog.prototype, "open", "open_finish");
 Gio._promisify(
@@ -29,47 +30,37 @@ select_mode.connect("notify::selected-item", () => {
   console.log(`Mode: ${selected_mode}`);
 });
 
-const dialog_for_file = new Gtk.FileDialog({
-  title: _("Select File(s)"),
-  modal: true,
-});
-
-const dialog_for_folder = new Gtk.FileDialog({
-  title: _("Select Folder(s)"),
-  modal: true,
-});
-
-const dialog_for_save = new Gtk.FileDialog({
-  title: _("Save File"),
-  modal: true,
-});
-
 button.connect("clicked", () => {
   switch (selected_mode) {
     case "Select File":
-      open_file().catch(logError);
-      break;
-    case "Select File (Folder pre-defined)":
-      open_file_folder().catch(logError);
+      openFile().catch(logError);
       break;
     case "Select Multiple Files":
-      open_multiple_files().catch(logError);
+      openMultipleFiles().catch(logError);
       break;
     case "Select Folder":
-      select_folder().catch(logError);
+      selectFolder().catch(logError);
       break;
     case "Select Multiple Folders":
-      select_multiple_folders().catch(logError);
+      selectMultipleFolders().catch(logError);
       break;
     case "Save File":
-      save_file().catch(logError);
+      saveFile().catch(logError);
       break;
     default:
       console.log("Invalid mode selected.");
   }
 });
 
-async function open_file() {
+async function openFile() {
+  const default_dir = Gio.File.new_for_path(
+    GLib.get_user_special_dir(GLib.UserDirectory.DIRECTORY_DOWNLOAD),
+  );
+  const dialog_for_file = new Gtk.FileDialog({
+    title: _("Select File"),
+    modal: true,
+    initial_folder: default_dir,
+  });
   const file = await dialog_for_file.open(workbench.window, null);
   const info = file.query_info(
     "standard::*",
@@ -79,28 +70,24 @@ async function open_file() {
   console.log(`Selected File: ${info.get_name()}`);
 }
 
-async function open_file_folder() {
-  const folder = Gio.File.new_for_path(pkg.pkgdatadir).resolve_relative_path(
-    "Library/demos/File Dialog/main.blp",
-  );
-  const parent_folder = folder.get_parent();
-  dialog_for_file.initial_folder = parent_folder;
-  const file = await dialog_for_file.open(workbench.window, null);
-  const info = file.query_info(
-    "standard::*",
-    Gio.FileQueryInfoFlags.NOFOLLOW_SYMLINKS,
+async function openMultipleFiles() {
+  const dialog_for_multiple_files = new Gtk.FileDialog({
+    title: _("Select File(s)"),
+    modal: true,
+  });
+  const files = await dialog_for_multiple_files.open_multiple(
+    workbench.window,
     null,
   );
-  console.log(`Selected File: ${info.get_name()}`);
-}
-
-async function open_multiple_files() {
-  const files = await dialog_for_file.open_multiple(workbench.window, null);
   const selected_items_count = files.get_n_items();
   console.log(`No of selected files: ${selected_items_count}`);
 }
 
-async function select_folder() {
+async function selectFolder() {
+  const dialog_for_folder = new Gtk.FileDialog({
+    title: _("Select Folder"),
+    modal: true,
+  });
   const folder = await dialog_for_folder.select_folder(workbench.window, null);
   const info = folder.query_info(
     "standard::*",
@@ -110,8 +97,12 @@ async function select_folder() {
   console.log(`Selected Folder: ${info.get_name()}`);
 }
 
-async function select_multiple_folders() {
-  const folders = await dialog_for_folder.select_multiple_folders(
+async function selectMultipleFolders() {
+  const dialog_for_multiple_folders = new Gtk.FileDialog({
+    title: _("Select Folder(s)"),
+    modal: true,
+  });
+  const folders = await dialog_for_multiple_folders.select_multiple_folders(
     workbench.window,
     null,
   );
@@ -119,7 +110,11 @@ async function select_multiple_folders() {
   console.log(`No of selected folders: ${selected_items_count}`);
 }
 
-async function save_file() {
+async function saveFile() {
+  const dialog_for_save = new Gtk.FileDialog({
+    title: _("Save File"),
+    modal: true,
+  });
   const file = await dialog_for_save.save(workbench.window, null);
   console.log("Operation complete");
 }
