5d75f3148dd8d673dbd4c551c593a1a4c6f44860
Sonny Piers <sonny@fastmail.net>
2023-07-16 14:13:34 +0200

    Multi windows / sessions support (#363)

--- src/Library/Library.js
+++ src/Library/Library.js
@@ -1,21 +1,14 @@
 import Gio from "gi://Gio";
-import GLib from "gi://GLib";
 import Adw from "gi://Adw";
 import Gtk from "gi://Gtk";
 
-import { decode } from "../util.js";
+import { demos_dir, readDemoFile } from "../util.js";
+import Window from "../window.js";
 
 import resource from "./Library.blp";
+import { createSessionFromDemo } from "../sessions.js";
 
-const demo_dir = Gio.File.new_for_path(
-  GLib.build_filenamev([pkg.pkgdatadir, "Library/demos"]),
-);
-
-export default function Library({
-  openDemo,
-  window: application_window,
-  application,
-}) {
+export default function Library({ application }) {
   const builder = Gtk.Builder.new_from_resource(resource);
   const window = builder.get_object("library");
 
@@ -36,11 +29,8 @@ export default function Library({
     );
     widget.connect("activated", () => {
       last_selected = widget;
-      openDemo(demo.name)
-        .then(() => {
-          application_window.present();
-        })
-        .catch(logError);
+
+      openDemo({ application, demo_name: demo.name }).catch(logError);
     });
 
     builder.get_object(`library_${demo.category}`).add(widget);
@@ -54,25 +44,14 @@ export default function Library({
     window.present();
     last_selected?.grab_focus();
   });
-  application_window.add_action(action_library);
-  application.set_accels_for_action("win.library", ["<Control><Shift>O"]);
-}
-
-export function readDemo(demo_name) {
-  const javascript = readDemoFile(demo_name, "main.js");
-  const css = readDemoFile(demo_name, "main.css");
-  const xml = readDemoFile(demo_name, "main.ui");
-  const blueprint = readDemoFile(demo_name, "main.blp");
-  const vala = readDemoFile(demo_name, "main.vala");
-  const json = JSON.parse(readDemoFile(demo_name, "main.json"));
-
-  return { ...json, javascript, css, xml, blueprint, vala };
+  application.add_action(action_library);
+  application.set_accels_for_action("app.library", ["<Control><Shift>O"]);
 }
 
 function getDemos() {
   const demos = [];
 
-  for (const child of demo_dir.enumerate_children(
+  for (const child of demos_dir.enumerate_children(
     "",
     Gio.FileQueryInfoFlags.NOFOLLOW_SYMLINKS,
     null,
@@ -102,19 +81,12 @@ function getDemos() {
   return demos;
 }
 
-function readDemoFile(demo_name, file_name) {
-  const file = demo_dir.resolve_relative_path(`${demo_name}/${file_name}`);
+async function openDemo({ application, demo_name }) {
+  const demo = JSON.parse(readDemoFile(demo_name, "main.json"));
+  const session = await createSessionFromDemo(demo);
 
-  let str;
+  const is_js = session.file.get_child("main.js").query_exists(null);
 
-  try {
-    str = decode(file.load_contents(null)[1]);
-  } catch (err) {
-    if (err.code !== Gio.IOErrorEnum.NOT_FOUND) {
-      throw err;
-    }
-    str = "";
-  }
-
-  return str;
+  const { load } = Window({ application, session });
+  await load({ run: demo.autorun && is_js });
 }
