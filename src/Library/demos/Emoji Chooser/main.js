fec3d09cccdd6257a0bedb5c4bf5108049266f0f
AkshayWarrier <aksmen121@gmail.com>
2023-07-16 09:09:57 +0530

    Emoji Chooser: minor change

--- src/Library/demos/Emoji Chooser/main.js
+++ src/Library/demos/Emoji Chooser/main.js
@@ -1,6 +1,3 @@
-import Gtk from "gi://Gtk";
-import Adw from "gi://Adw";
-
 const emoji_chooser = workbench.builder.get_object("emoji_chooser");
 const button = workbench.builder.get_object("button");
 
