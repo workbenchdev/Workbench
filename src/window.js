d60e4f6f7aae811cf2d12ad5e914bfe41f130588
Diego Iván <diegoivan.mae@gmail.com>
2023-07-26 16:19:03 -0600

    sessions,window: Set current demo's name as title
    This will also store the demo name inside the session's settings so it
    can also be restored :)

--- src/window.js
+++ src/window.js
@@ -56,6 +56,7 @@ export default function Window({ application, session }) {
     window.add_css_class("devel");
   }
   window.set_application(application);
+  window.set_title(`Workbench — ${settings.get_string("title")}`);
 
   // Popover menu theme switcher
   const button_menu = builder.get_object("button_menu");
