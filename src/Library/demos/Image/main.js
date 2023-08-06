453987eb3fe63b9fc96019b751f352f40923c72a
Sonny Piers <sonny@fastmail.net>
2023-08-07 00:37:51 +0200

    update

--- src/Library/demos/Image/main.js
+++ src/Library/demos/Image/main.js
@@ -1,13 +1,10 @@
 import Gio from "gi://Gio";
 
-const icon1 = workbench.builder.get_object("icon1");
-const icon2 = workbench.builder.get_object("icon2");
-const icon3 = workbench.builder.get_object("icon3");
+const path = Gio.File.new_for_uri(
+  workbench.resolve("workbench.png"),
+).get_path();
 
-const file = Gio.File.new_for_path(pkg.pkgdatadir).resolve_relative_path(
-  "Library/demos/Image/re.sonny.Workbench.png",
-);
+workbench.builder.get_object("icon1").file = path;
+workbench.builder.get_object("icon2").file = path;
+workbench.builder.get_object("icon3").file = path;
 
-icon1.file = file.get_path();
-icon2.file = file.get_path();
-icon3.file = file.get_path();
