76f258f34fb2afd16deb2032b8ed75b22c2d710b
Sriyansh Shivam <sriyanshshivam1@gmail.com>
2023-08-12 17:53:54 +0530

    Applied Changes

--- src/Library/demos/Navigation View/main.js
+++ src/Library/demos/Navigation View/main.js
@@ -1,9 +1,3 @@
-import Gio from "gi://Gio";
-import Gdk from "gi://Gdk";
-import GObject from "gi://GObject";
-import Gtk from "gi://Gtk";
-import Adw from "gi://Adw";
-
 const nav_view = workbench.builder.get_object("nav_view");
 const nav_pageone = workbench.builder.get_object("nav_pageone");
 const next_button = workbench.builder.get_object("next_button");
@@ -11,28 +5,55 @@ const previous_button = workbench.builder.get_object("previous_button");
 const nav_pagetwo = workbench.builder.get_object("nav_pagetwo");
 const nav_pagethree = workbench.builder.get_object("nav_pagethree");
 const nav_pagefour = workbench.builder.get_object("nav_pagefour");
-const decisive_button = workbench.builder.get_object("decisive_button");
+const decisive_button_transition = workbench.builder.get_object(
+  "decisive_button_transition",
+);
+const decisive_button_poponescape = workbench.builder.get_object(
+  "decisive_button_poponescape",
+);
+const title = workbench.builder.get_object("title");
 
 next_button.connect("clicked", () => {
-  previous_button.sensitive = true;
-  if (nav_view.get_visible_page() === nav_pageone) {
-    nav_view.push(nav_pagetwo);
-  } else if (nav_view.get_visible_page() === nav_pagetwo) {
-    nav_view.push(nav_pagethree);
-  } else if (nav_view.get_visible_page() === nav_pagethree) {
-    nav_view.push(nav_pagefour);
-    next_button.sensitive = false;
+  switch (nav_view.visible_page) {
+    case nav_pageone:
+      nav_view.push(nav_pagetwo);
+      title.label = "Page 2";
+      break;
+    case nav_pagetwo:
+      nav_view.push(nav_pagethree);
+      title.label = "Page 3";
+      break;
+    case nav_pagethree:
+      nav_view.push(nav_pagefour);
+      title.label = "Page 4";
+      break;
   }
 });
 
 previous_button.connect("clicked", () => {
   nav_view.pop();
-  next_button.sensitive = true;
-  if (nav_view.get_visible_page() === nav_pageone) {
-    previous_button.sensitive = false;
+  switch (nav_view.visible_page) {
+    case nav_pageone:
+      title.label = "Page 1";
+      break;
+    case nav_pagetwo:
+      title.label = "Page 2";
+      break;
+    case nav_pagethree:
+      title.label = "Page 3";
+      break;
   }
 });
 
-decisive_button.connect("notify::active", () => {
-  nav_view.animate_transitions = decisive_button.active;
+decisive_button_transition.connect("notify::active", () => {
+  nav_view.animate_transitions = decisive_button_transition.active;
+});
+
+decisive_button_poponescape.connect("notify::active", () => {
+  nav_view.pop_on_escape = decisive_button_poponescape.active;
+});
+
+nav_view.connect("notify::visible-page", () => {
+  previous_button.sensitive = nav_view.visible_page !== nav_pageone;
+  next_button.sensitive = nav_view.visible_page !== nav_pagefour;
 });
