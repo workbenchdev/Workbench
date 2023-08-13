0ab2be123862a5210bb6d8a850ffb3ce0b6ab6c8
Sonny Piers <sonny@fastmail.net>
2023-08-13 16:51:38 +0200

    improvements

--- src/Library/demos/Source View/main.js
+++ src/Library/demos/Source View/main.js
@@ -1,5 +1,3 @@
-import Gtk from "gi://Gtk";
-import Adw from "gi://Adw";
 import GtkSource from "gi://GtkSource";
 
 // Strictly speaking we don't _have_ to do this here since WorkBench does this for us.
@@ -9,17 +7,19 @@ GtkSource.init();
 // Get the language we want to use
 const language_manager = GtkSource.LanguageManager.get_default();
 const language = language_manager.get_language("js");
-	  // Create the buffer - this holds the text that's used in the SourceView
+
+// Create the buffer - this holds the text that's used in the SourceView
 const buffer = GtkSource.Buffer.new_with_language(language);
 buffer.set_text('console.log("Hello World!");', -1);
 
 // Create the SourceView which displays the buffer's display
-const view = GtkSource.View.new_with_buffer(buffer);
-view.set_auto_indent(true);
-view.set_indent_width = 4;
-view.show_line_numbers = true;
+const source_view = GtkSource.View.new({
+  auto_indent: true,
+  indent_width: 4,
+  buffer,
+  show_line_numbers: true,
+});
 
 // Add the SourceView to our ScrolledView so its displayed
-const sw = workbench.builder.get_object("sw");
-sw.set_child(view);
-
+const scrolled_window = workbench.builder.get_object("scrolled_window");
+scrolled_window.set_child(source_view);
