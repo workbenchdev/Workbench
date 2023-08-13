e219470f62107ed03a5942acb9b6fa02e296e48e
Sonny Piers <sonny@fastmail.net>
2023-08-13 16:04:45 +0200

    small improvements

--- src/Library/demos/Power Profile Monitor/main.js
+++ src/Library/demos/Power Profile Monitor/main.js
@@ -5,12 +5,21 @@ const power_profile_monitor = Gio.PowerProfileMonitor.dup_default();
 const overlay = workbench.builder.get_object("overlay");
 
 power_profile_monitor.connect("notify::power-saver-enabled", () => {
+  let toast;
+
   if (power_profile_monitor.power_saver_enabled) {
-    const toast = new Adw.Toast({
-      title: "Paused to save power",
+    toast = new Adw.Toast({
+      title: "Backup paused to save power",
       button_label: "Resume",
       priority: Adw.ToastPriority.HIGH,
     });
-    overlay.add_toast(toast);
+  } else {
+    toast = new Adw.Toast({
+      title: "Backup resumed",
+      priority: Adw.ToastPriority.HIGH,
+    });
   }
+
+  overlay.add_toast(toast);
 });
+
