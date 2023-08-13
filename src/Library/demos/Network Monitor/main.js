98631c1107a5f9ab2a55e8c9cf4d880fb9fe0c1a
Sonny Piers <sonny@fastmail.net>
2023-08-13 16:12:30 +0200

    improvements

--- src/Library/demos/Network Monitor/main.js
+++ src/Library/demos/Network Monitor/main.js
@@ -5,13 +5,17 @@ const network_monitor = Gio.NetworkMonitor.get_default();
 const scale = workbench.builder.get_object("scale");
 const level_bar = workbench.builder.get_object("level_bar");
 
-level_bar.value = network_monitor.connectivity;
-
-network_monitor.connect("network-changed", () => {
+function setNetworkStatus() {
   banner.revealed = network_monitor.network_metered;
   level_bar.value = network_monitor.connectivity;
+}
+
+setNetworkStatus();
+network_monitor.connect("network-changed", () => {
+  setNetworkStatus();
 });
 
 banner.connect("button-clicked", () => {
   banner.revealed = false;
 });
+
