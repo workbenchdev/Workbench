a5a8636d772457ffec4c6e23c44b7481a2b98c14
halfmexican <103920890+halfmexican@users.noreply.github.com>
2023-08-13 14:02:01 -0500

    library: add Breakpoint entry (#484)

---
+++ src/Library/demos/Breakpoints/main.js
@@ -0,0 +1,11 @@
+import Gtk from "gi://Gtk";
+
+const breakpoint = workbench.builder.get_object("breakpoint");
+
+breakpoint.connect("apply", () => {
+  console.log("Breakpoint Applied");
+});
+
+breakpoint.connect("unapply", () => {
+  console.log("Breakpoint Unapplied");
+});
