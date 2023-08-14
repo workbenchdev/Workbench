79778467ecf7304ebf321f87d65805184798a985
halfmexican <halfmexicanhalfamazing@gmail.com>
2023-08-13 19:25:17 -0500

    stuff

--- src/Library/demos/Picture/main.js
+++ src/Library/demos/Picture/main.js
@@ -3,6 +3,7 @@ import Gio from "gi://Gio";
 const picture1 = workbench.builder.get_object("picture1");
 const picture2 = workbench.builder.get_object("picture2");
 const picture3 = workbench.builder.get_object("picture3");
+
 const file1 = Gio.File.new_for_uri(workbench.resolve("./blobs.png"));
 const file2 = Gio.File.new_for_uri(workbench.resolve("./drool.png"));
 const file3 = Gio.File.new_for_uri(workbench.resolve("./keys.png"));
