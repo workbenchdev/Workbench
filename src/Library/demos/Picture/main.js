e97d4293cb72510eec833260952b2a11f48aaf86
halfmexican <halfmexicanhalfamazing@gmail.com>
2023-08-14 18:32:33 -0500

    main.js: use only keys.png

--- src/Library/demos/Picture/main.js
+++ src/Library/demos/Picture/main.js
@@ -4,10 +4,8 @@ const picture1 = workbench.builder.get_object("picture1");
 const picture2 = workbench.builder.get_object("picture2");
 const picture3 = workbench.builder.get_object("picture3");
 
-const file1 = Gio.File.new_for_uri(workbench.resolve("./blobs.png"));
-const file2 = Gio.File.new_for_uri(workbench.resolve("./drool.png"));
-const file3 = Gio.File.new_for_uri(workbench.resolve("./keys.png"));
+const file = Gio.File.new_for_uri(workbench.resolve("./keys.png"));
 
-picture1.file = file1;
-picture2.file = file2;
-picture3.file = file3;
+picture1.file = file;
+picture2.file = file;
+picture3.file = file;
