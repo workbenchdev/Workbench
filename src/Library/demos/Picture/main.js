06f17c4c8921b725962325f97c77e0d62ab64d4b
halfmexican <halfmexicanhalfamazing@gmail.com>
2023-08-13 19:18:37 -0500

    main.js: use new relative path api

--- src/Library/demos/Picture/main.js
+++ src/Library/demos/Picture/main.js
@@ -1,11 +1,12 @@
 import Gio from "gi://Gio";
 
-const picture = workbench.builder.get_object("picture");
-const content_fit_row = workbench.builder.get_object("content_fit_row");
-const file = Gio.File.new_for_path(pkg.pkgdatadir).resolve_relative_path(
-  "Library/demos/Picture/blobs.png",
-);
+const picture1 = workbench.builder.get_object("picture1");
+const picture2 = workbench.builder.get_object("picture2");
+const picture3 = workbench.builder.get_object("picture3");
+const file1 = Gio.File.new_for_uri(workbench.resolve("./blobs.png"));
+const file2 = Gio.File.new_for_uri(workbench.resolve("./drool.png"));
+const file3 = Gio.File.new_for_uri(workbench.resolve("./keys.png"));
 
-picture.file = file;
-picture.content_fit = content_fit_row.selected;
-
+picture1.file = file1;
+picture2.file = file2;
+picture3.file = file3;
