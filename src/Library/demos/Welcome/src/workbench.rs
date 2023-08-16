6472fdada2e26c91e6e030bccafb4076fc16cff3
Hofer-Julian <30049909+Hofer-Julian@users.noreply.github.com>
2023-08-16 10:16:20 +0200

    Update workbench.rs

--- src/Library/demos/Welcome/src/workbench.rs
+++ src/Library/demos/Welcome/src/workbench.rs
@@ -14,7 +14,7 @@ pub(crate) fn window() -> &'static gtk::Window {
     unsafe {
         crate::WINDOW
             .as_ref()
-            .expect("Builder instance should already be initialized.")
+            .expect("Window instance should already be initialized.")
     }
 }
 
