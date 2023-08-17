df97541c491c2176d6356f12ef9582f1a3bbb550
AkshayWarrier <aksmen121@gmail.com>
2023-08-18 00:20:06 +0530

    Session Monitor: Add other session change inhibits

--- src/Library/demos/Session Monitor/main.js
+++ src/Library/demos/Session Monitor/main.js
@@ -17,8 +17,13 @@ Gio._promisify(
 const portal = new Xdp.Portal();
 const parent = XdpGtk.parent_new_gtk(workbench.window);
 const entry = workbench.builder.get_object("entry");
+const check_logout = workbench.builder.get_object("logout");
+const check_user_switch = workbench.builder.get_object("user_switch");
+const check_suspend = workbench.builder.get_object("suspend");
+const check_idle = workbench.builder.get_object("idle");
 const button_start = workbench.builder.get_object("button_start");
 const button_stop = workbench.builder.get_object("button_stop");
+let ids = [];
 
 button_start.connect("clicked", () => {
   startSession().catch(logError);
@@ -31,10 +36,20 @@ button_stop.connect("clicked", () => {
 portal.connect(
   "session-state-changed",
   (self, screensaver_active, session_state) => {
-    if (session_state === Xdp.LoginSessionState.QUERY_END) {
-      console.log("User is logging out");
-      portal.session_monitor_query_end_response();
-      inhibitSession(Xdp.InhibitFlags.LOGOUT);
+    if (screensaver_active) {
+      console.log("Screensaver is active");
+    }
+    switch (session_state) {
+      case Xdp.LoginSessionState.RUNNING:
+        console.log("Session: Running");
+        break;
+      case Xdp.LoginSessionState.QUERY_END:
+        console.log("Session: Query End");
+        portal.session_monitor_query_end_response();
+        break;
+      case Xdp.LoginSessionState.ENDING:
+        console.log("Session: Ending");
+        break;
     }
   },
 );
@@ -44,10 +59,16 @@ async function startSession() {
   if (result) {
     button_start.sensitive = false;
     button_stop.sensitive = true;
+    if (check_logout.active) inhibitSession(Xdp.InhibitFlags.LOGOUT);
+    if (check_user_switch.active) inhibitSession(Xdp.InhibitFlags.USER_SWITCH);
+    if (check_suspend.active) inhibitSession(Xdp.InhibitFlags.SUSPEND);
+    if (check_idle.active) inhibitSession(Xdp.InhibitFlags.IDLE);
   }
 }
 
 function stopSession() {
+  for (const id of ids) portal.session_uninhibit(id);
+  ids = [];
   portal.session_monitor_stop();
   button_start.sensitive = true;
   button_stop.sensitive = false;
@@ -56,6 +77,6 @@ function stopSession() {
 async function inhibitSession(flag) {
   const reason = entry.text;
   const id = await portal.session_inhibit(parent, reason, flag, null);
-  log(id);
+  ids.push(id);
 }
 
