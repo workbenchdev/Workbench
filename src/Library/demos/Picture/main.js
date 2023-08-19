85f4e812b7af78775c5da6121b44a850e33ca913
Sonny Piers <sonny@fastmail.net>
2023-08-19 13:45:29 +0200

    fixes

--- src/Library/demos/Picture/main.js
+++ src/Library/demos/Picture/main.js
@@ -1,11 +1,14 @@
 import Gio from "gi://Gio";
 
-const picture1 = workbench.builder.get_object("picture1");
-const picture2 = workbench.builder.get_object("picture2");
-const picture3 = workbench.builder.get_object("picture3");
+const picture_fill = workbench.builder.get_object("picture_fill");
+const picture_contain = workbench.builder.get_object("picture_contain");
+const picture_cover = workbench.builder.get_object("picture_cover");
+const picture_scale_down = workbench.builder.get_object("picture_scale_down");
 
 const file = Gio.File.new_for_uri(workbench.resolve("./keys.png"));
 
-picture1.file = file;
-picture2.file = file;
-picture3.file = file;
+picture_fill.file = file;
+picture_contain.file = file;
+picture_cover.file = file;
+picture_scale_down.file = file;
+
