6f4c5082be39df2b16e6573cf9e4f27ef57ce3bb
AkshayWarrier <58233418+AkshayWarrier@users.noreply.github.com>
2023-08-19 07:41:33 +0530

    library: Add Memory Monitor entry (#508)
    * library: Add Memory Monitor entry
    
    * Update main.json
    
    * Update main.blp
    
    * Memory Monitor: Suggestions

---
+++ src/Library/demos/Memory Monitor/main.js
@@ -0,0 +1,27 @@
+import Gio from "gi://Gio";
+
+const memory_monitor = Gio.MemoryMonitor.dup_default();
+
+const cache = new Map();
+cache.set("a", 1);
+cache.set("b", 2);
+cache.set("c", 3);
+
+memory_monitor.connect("low-memory-warning", (monitor, level) => {
+  // Use inequalities for checking as new levels may be added in the future
+  if (level >= Gio.MemoryMonitorWarningLevel.LOW) {
+    // Processes should free up unneeded resources
+    console.log("Warning Level: Low");
+    drop_caches();
+  } else if (level >= Gio.MemoryMonitorWarningLevel.MEDIUM) {
+    // Processes should try harder to free up unneeded resources
+    console.log("Warning Level: Medium");
+  } else if (level >= Gio.MemoryMonitorWarningLevel.CRITICAL) {
+    // system will start terminating processes to reclaim memory
+    console.log("Warning Level: Critical");
+  }
+});
+
+function drop_caches() {
+  cache.clear();
+}
