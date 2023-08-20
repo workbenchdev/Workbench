b0033e94abb4bbc8c3a99d28852510a10464dff5
AkshayWarrier <58233418+AkshayWarrier@users.noreply.github.com>
2023-08-20 20:19:46 +0530

    Memory Monitor: Change else ifs to ifs

--- src/Library/demos/Memory Monitor/main.js
+++ src/Library/demos/Memory Monitor/main.js
@@ -13,10 +13,12 @@ memory_monitor.connect("low-memory-warning", (monitor, level) => {
     // Processes should free up unneeded resources
     console.log("Warning Level: Low");
     drop_caches();
-  } else if (level >= Gio.MemoryMonitorWarningLevel.MEDIUM) {
+  }
+  if (level >= Gio.MemoryMonitorWarningLevel.MEDIUM) {
     // Processes should try harder to free up unneeded resources
     console.log("Warning Level: Medium");
-  } else if (level >= Gio.MemoryMonitorWarningLevel.CRITICAL) {
+  }
+  if (level >= Gio.MemoryMonitorWarningLevel.CRITICAL) {
     // system will start terminating processes to reclaim memory
     console.log("Warning Level: Critical");
   }
