d2a4345ca5d71f43f179bfbf6a5d0671f702b1e4
Sriyansh Shivam <sriyanshshivam1@gmail.com>
2023-08-22 19:31:00 +0530

    Applied Changes

--- src/Library/demos/File Monitor/main.js
+++ src/Library/demos/File Monitor/main.js
@@ -7,7 +7,6 @@ Gio._promisify(Gtk.FileLauncher.prototype, "launch", "launch_finish");
 
 const edit_entry = workbench.builder.get_object("edit_entry");
 const view_file = workbench.builder.get_object("view_file");
-const create_file = workbench.builder.get_object("create_file");
 const delete_file = workbench.builder.get_object("delete_file");
 const edit_file = workbench.builder.get_object("edit_file");
 const file_name = workbench.builder.get_object("file_name");
@@ -25,54 +24,51 @@ const details = file.query_info(
   null,
 );
 file_name.label = details.get_display_name();
-buffer.set_text("Start editing ... ", -1);
+buffer.text = "Start editing ... ";
 
 const monitor_for_dir = file_dir.monitor(
   Gio.FileMonitorFlags.WATCH_MOVES,
   null,
 );
-const monitor_for_file = file.monitor(Gio.FileMonitorFlags.WATCH_MOVES, null);
+const monitor_for_file = file.monitor(Gio.FileMonitorFlags.NONE, null);
 
 delete_file.connect("clicked", () => {
   file.delete_async(GLib.PRIORITY_DEFAULT, null).catch(logError);
-});
-
-create_file.connect("clicked", () => {
-  const new_file = file_dir.get_child("new-file.txt");
-  new_file
-    .create_async(Gio.FileCreateFlags.NONE, GLib.PRIORITY_DEFAULT, null, null)
-    .catch(logError);
 });
 
 view_file.connect("clicked", () => {
   file_launcher.launch(workbench.window, null).catch(logError);
 });
 
-monitor_for_file.connect("changed", () => {
-  const toast = new Adw.Toast({
-    title: "File modified",
-    timeout: 2,
-  });
-  overlay.add_toast(toast);
+monitor_for_file.connect("changed", (monitor, file, other_file, event) => {
+  console.log(event);
+  if (event === Gio.FileMonitorEvent.CHANGES_DONE_HINT) {
+    const toast = new Adw.Toast({
+      title: "File modified",
+      timeout: 2,
+    });
+    overlay.add_toast(toast);
+  }
 });
 
 monitor_for_dir.connect("changed", (monitor, child, other_file, event) => {
   const toast = new Adw.Toast({
-    title: "Unknown Event",
     timeout: 2,
   });
+
   switch (event) {
     case Gio.FileMonitorEvent.RENAMED:
       toast.title = `${child.get_basename()} was renamed to ${other_file.get_basename()}`;
       break;
-    case Gio.FileMonitorEvent.MOVED_IN:
-      toast.title = `${child.get_basename()} was moved into the directory`;
+    case Gio.FileMonitorEvent.DELETED:
+      toast.title = `${child.get_basename()} was deleted from the directory`;
       break;
-    case Gio.FileMonitorEvent.MOVED_OUT:
-      toast.title = `${child.get_basename()} was moved out of the directory`;
+    case Gio.FileMonitorEvent.CREATED:
+      toast.title = `${child.get_basename()} created in the directory`;
       break;
   }
-  overlay.add_toast(toast);
+
+  if (toast.title) overlay.add_toast(toast);
 });
 
 edit_file.connect("clicked", () => {
