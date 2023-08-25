ed6c6f1d56ebde070853ab0934acef648af653ef
halfmexican <halfmexicanhalfamazing@gmail.com>
2023-08-24 19:15:58 -0500

    update main.js

--- src/Library/demos/Spell Checker/main.js
+++ src/Library/demos/Spell Checker/main.js
@@ -5,22 +5,15 @@ import Adw from "gi://Adw";
 GtkSource.init();
 
 const buffer = workbench.builder.get_object("buffer");
-const source_view = workbench.builder.get_object("source_view");
-const style_manager = Adw.StyleManager.get_default();
-const scheme_manager = GtkSource.StyleSchemeManager.get_default();
-const scheme = scheme_manager.get_scheme(
-  style_manager.dark ? "Adwaita-dark" : "Adwaita",
-);
-
-buffer.style_scheme = scheme;
+const text_view = workbench.builder.get_object("text_view");
 
 // Spell checking setup
 const checker = Spelling.Checker.get_default();
+checker.set_language("en_US"); // set to U.S English
 const adapter = Spelling.TextBufferAdapter.new(buffer, checker);
 const extra_menu = adapter.get_menu_model();
 
-source_view.set_extra_menu(extra_menu);
-source_view.insert_action_group("spelling", adapter);
+text_view.set_extra_menu(extra_menu);
+text_view.insert_action_group("spelling", adapter);
 
 adapter.set_enabled(true);
-
