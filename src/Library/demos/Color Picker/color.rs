b0d8977527151999a7afcaf48fc375b5850070b1
Julian Hofer <julianhofer@gnome.org>
2023-08-27 15:37:44 +0200

    Add color picker demo

---
+++ src/Library/demos/Color Picker/color.rs
@@ -0,0 +1,30 @@
+use crate::workbench;
+use ashpd::desktop::screenshot::Color;
+use glib::MainContext;
+use gtk::prelude::*;
+use gtk::{gdk, glib};
+
+pub fn main() {
+    let button: gtk::Button = workbench::builder().object("button").unwrap();
+    button.connect_clicked(|_| {
+        MainContext::default().spawn_local(async {
+            if let Err(err) = pick_color().await {
+                eprint!("Could not request user information: {err}")
+            }
+        });
+    });
+}
+
+async fn pick_color() -> ashpd::Result<()> {
+    let color = Color::request().send().await?.response()?;
+
+    let color = gdk::RGBA::new(
+        color.red() as f32,
+        color.green() as f32,
+        color.blue() as f32,
+        1.0,
+    );
+    println!("Selected color is {color}");
+    Ok(())
+}
+
