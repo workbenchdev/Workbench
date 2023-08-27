12048cbd256b29141d0a728c4850a9ed0f4a9507
Julian Hofer <julianhofer@gnome.org>
2023-08-27 15:48:39 +0200

    Add email demo

---
+++ src/Library/demos/Email/code.rs
@@ -0,0 +1,34 @@
+use crate::workbench;
+use ashpd::desktop::email::EmailRequest;
+use glib::MainContext;
+use gtk::glib;
+use gtk::prelude::*;
+
+pub fn main() {
+    let button: gtk::Button = workbench::builder().object("button").unwrap();
+    button.connect_clicked(|_| {
+        MainContext::default().spawn_local(async {
+            if let Err(err) = send_email().await {
+                eprintln!("{err}")
+            }
+        });
+    });
+}
+
+async fn send_email() -> ashpd::Result<()> {
+    let entry: gtk::Entry = workbench::builder().object("entry").unwrap();
+    let request = EmailRequest::default()
+        .address(entry.text().as_str())
+        .subject("Email from Workbench")
+        .body("Hello World!")
+        .send()
+        .await?;
+
+    if request.response().is_ok() {
+        println!("Success");
+    } else {
+        eprintln!("Failure, verify that you have an email application.");
+    }
+    Ok(())
+}
+
