69168af6657a12d2559748957f3ca6f239231845
Saad Khan <skhan.bee20seecs@seecs.edu.pk>
2023-08-28 22:58:49 +0500

    Fixed memory management issues

--- src/Library/demos/Tab View/code.rs
+++ src/Library/demos/Tab View/code.rs
@@ -1,6 +1,11 @@
 use crate::workbench;
 use adw::prelude::*;
 
+use crate::glib::clone;
+use gtk::glib;
+use std::cell::Cell;
+use std::rc::Rc;
+
 pub fn main() {
     let tab_view: adw::TabView = workbench::builder()
         .object("tab_view")
@@ -14,24 +19,24 @@ pub fn main() {
     let button_overview: gtk::Button = workbench::builder()
         .object("button_overview")
         .unwrap();
-    let mut tab_count = 1;
+    let tab_count = Rc::new(Cell::new(1));
 
-    button_new_tab.connect_clicked(move |_| {
-        add_page(&tab_view, &mut tab_count);
-    });
-    overview.connect_create_tab(move |_| add_page(&tab_view, &mut tab_count));
+    button_new_tab.connect_clicked(clone!(@weak tab_view, @weak tab_count => move |_| {
+        add_page(&tab_view, &tab_count);
+    }));
+    overview.connect_create_tab(move |_| add_page(&tab_view, &tab_count));
     button_overview.connect_clicked(move |_| overview.set_open(true));
 }
 
-fn add_page(tab_view: &adw::TabView, tab_count: &mut u16) -> adw::TabPage {
-    let title = format!("Tab {}", tab_count);
+fn add_page(tab_view: &adw::TabView, tab_count: &Rc<Cell<i32>>) -> adw::TabPage {
+    let title = format!("Tab {}", tab_count.get());
     let page = create_page(&title);
     let tab_page = tab_view.append(&page);
 
     tab_page.set_title(&title);
     tab_page.set_live_thumbnail(true);
 
-    *tab_count += 1;
+    tab_count.set(tab_count.get() + 1);
     tab_page
 }
 
