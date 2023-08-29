bc357b2674a4099212d736f25252f865951da9c9
Muhammad Bilal <111046732+mbilal234@users.noreply.github.com>
2023-08-29 20:59:46 +0500

    Update src/Library/demos/Separator/code.rs
    Co-authored-by: Hofer-Julian <30049909+Hofer-Julian@users.noreply.github.com>

--- src/Library/demos/Separator/code.rs
+++ src/Library/demos/Separator/code.rs
@@ -5,7 +5,7 @@ pub fn main() {
     let picture_one: gtk::Picture = workbench::builder().object("picture_one").unwrap();
     let picture_two: gtk::Picture = workbench::builder().object("picture_two").unwrap();
 
-    let file = gio::File::for_uri(workbench::resolve("./image.png").as_str());
+    let file = gio::File::for_uri(&workbench::resolve("./image.png"));
 
     picture_one.set_file(Some(&file));
     picture_two.set_file(Some(&file));
