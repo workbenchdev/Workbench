5e13e11e796cd2e70cdeee0c95c2ee5b9b73c324
Muhammad Bilal <111046732+mbilal234@users.noreply.github.com>
2023-08-29 18:42:41 +0500

    Added correct method in code.rs

--- src/Library/demos/Link Button/code.rs
+++ src/Library/demos/Link Button/code.rs
@@ -1,16 +1,18 @@
 use crate::workbench;
 use gtk::prelude::*;
+use gtk::traits::ButtonExt;
 
 pub fn main() {
     let linkbutton: gtk::LinkButton = workbench::builder().object("linkbutton").unwrap();
+
     linkbutton.connect_clicked(move |_| {
         println!("Link has been visited");
     });
 
-    linkbutton.connect_activate_link(|button| {
+    linkbutton.connect_notify(Some("visited"), |button, _| {
         println!("About to activate {}", button.uri());
-        // Return true if handling the link manually, or
+      // Return true if handling the link manually, or
         // false to let the default behavior continue
-        false.into()
+        false;
     });
 }
