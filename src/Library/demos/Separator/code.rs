3b68476313e5bd3badae2a01ea2007e362ab7e7e
Muhammad Bilal <111046732+mbilal234@users.noreply.github.com>
2023-08-29 19:52:45 +0500

    Create code.rs

---
+++ src/Library/demos/Separator/code.rs
@@ -0,0 +1,12 @@
+use crate::workbench;
+use gtk::gio;
+
+pub fn main() {
+    let picture_one: gtk::Picture = workbench::builder().object("picture_one").unwrap();
+    let picture_two: gtk::Picture = workbench::builder().object("picture_two").unwrap();
+
+    let file = gio::File::for_uri(workbench::resolve("./image.png").as_str());
+
+    picture_one.set_file(Some(&file));
+    picture_two.set_file(Some(&file));
+}
