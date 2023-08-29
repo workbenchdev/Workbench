5b3dcfc8f508ce62875f270a10a49c9c2ea6cc7b
Muhammad Bilal <111046732+mbilal234@users.noreply.github.com>
2023-08-29 20:27:44 +0500

    Create code.rs

---
+++ src/Library/demos/Menu Button/code.rs
@@ -0,0 +1,23 @@
+use crate::workbench;
+use gtk::prelude::*;
+use gtk::{Button, Switch};
+
+pub fn main() {
+    let circular_switch: Switch = workbench::builder()
+        .object("circular_switch")
+        .expect("Failed to get circular_switch");
+    let primary_button: Button = workbench::builder()
+        .object("primary")
+        .expect("Failed to get primary_button");
+    let secondary_button: Button = workbench::builder()
+        .object("secondary")
+        .expect("Failed to get secondary_button");
+
+    circular_switch.connect_active_notify(move |switch| {
+        if switch.is_active() {
+            secondary_button.add_css_class("circular");
+        } else {
+            secondary_button.remove_css_class("circular");
+        }
+    });
+}
