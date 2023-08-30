fe7c1649ae5b6f6bc446888f155a671b0ce01ff8
onkarrai06 <onkarrai06@gmail.com>
2023-08-30 17:58:13 +0500

    library: Ported ToggleButton to RUST

---
+++ src/Library/demos/Toggle Button/code.rs
@@ -0,0 +1,25 @@
+use crate::workbench;
+use gtk::prelude::*;
+use std::collections::HashMap;
+
+pub fn main() {
+    let buttons: HashMap<&str, &str> = [
+        ("button_no_look", "Don't look"),
+        ("button_look", "Look"),
+        ("button_camera", "Camera"),
+        ("button_flashlight", "Flashlight"),
+        ("button_console", "Console"),
+    ]
+    .iter()
+    .cloned()
+    .collect();
+
+    for (id, name) in buttons.iter() {
+        let button: gtk::ToggleButton = workbench::builder().object(*id).unwrap();
+        let cloned_name = name.to_string();
+        button.connect_active_notify(move |button| {
+            let status = if button.is_active() { "On" } else { "Off" };
+            println!("{} {}", cloned_name, status);
+        });
+    }
+}
