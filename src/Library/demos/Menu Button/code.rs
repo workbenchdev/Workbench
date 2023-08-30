cb015f956563ff8ecc116ffa2a29182671b3311b
Muhammad Bilal <111046732+mbilal234@users.noreply.github.com>
2023-08-30 15:17:14 +0500

    Update code.rs

--- src/Library/demos/Menu Button/code.rs
+++ src/Library/demos/Menu Button/code.rs
@@ -3,22 +3,14 @@ use gtk::prelude::*;
 use gtk::{MenuButton, Switch};
 
 pub fn main() {
-    let circular_switch: Switch = workbench
-      ::builder()
-      .object("circular_switch")
-      .unwrap();
-    let secondary_button: MenuButton = workbench
-      ::builder()
-      .object("secondary")
-      .unwrap();
+    let circular_switch: Switch = workbench::builder().object("circular_switch").unwrap();
+    let secondary_button: MenuButton = workbench::builder().object("secondary").unwrap();
 
     circular_switch.connect_active_notify(move |switch| {
         if switch.is_active() {
-            secondary_button
-              .add_css_class("circular");
+            secondary_button.add_css_class("circular");
         } else {
-            secondary_button
-              .remove_css_class("circular");
+            secondary_button.remove_css_class("circular");
         }
     });
 }
