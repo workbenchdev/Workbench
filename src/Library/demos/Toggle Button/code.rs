26cb34d99b1671cc8b9d7bd9acd556f3def5cec4
Onkar <121673391+onkarrai06@users.noreply.github.com>
2023-08-31 00:03:13 +0500

    library: Update code.rs

--- src/Library/demos/Toggle Button/code.rs
+++ src/Library/demos/Toggle Button/code.rs
@@ -3,23 +3,20 @@ use gtk::prelude::*;
 use std::collections::HashMap;
 
 pub fn main() {
-    let buttons: HashMap<&str, &str> = [
+    let buttons: HashMap<&str, &str> = HashMap::from([
         ("button_no_look", "Don't look"),
         ("button_look", "Look"),
         ("button_camera", "Camera"),
         ("button_flashlight", "Flashlight"),
         ("button_console", "Console"),
-    ]
-    .iter()
-    .cloned()
-    .collect();
+    ]);
 
-    for (id, name) in buttons.into_iter() {
-        let button: gtk::ToggleButton = workbench::builder().object(id).unwrap();
+    for (id, name) in buttons.iter() {
+        let button: gtk::ToggleButton = workbench::builder().object(*id).unwrap();
         let cloned_name = name.to_string();
         button.connect_active_notify(move |button| {
             let status = if button.is_active() { "On" } else { "Off" };
-            println!("{cloned_name} {status}");
+            println!("{} {}", cloned_name, status);
         });
     }
 }
