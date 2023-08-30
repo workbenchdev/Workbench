da04632a615dd206e6a7778c4c5ee741abae8902
Sonny Piers <sonny@fastmail.net>
2023-08-30 20:52:05 +0200

    f

--- src/Library/demos/Drawing Area/main.js
+++ src/Library/demos/Drawing Area/main.js
@@ -1,9 +1,7 @@
-import Cairo from "cairo";
-
 const drawingArea = workbench.builder.get_object("drawing_area");
 const scaleRotate = workbench.builder.get_object("scale");
 
-let triangle = [3];
+const triangle = [3];
 triangle[0] = [2];
 triangle[0][0] = 100;
 triangle[0][1] = 100;
@@ -13,9 +11,9 @@ triangle[1][1] = -100;
 triangle[2] = [2];
 triangle[2][0] = -100;
 triangle[2][1] = 100;
-let triangle_original = [3];
+const triangle_original = [3];
 for (let i = 0; i < 3; i++) {
-  let temp = [2];
+  const temp = [2];
   temp[0] = triangle[i][0];
   temp[1] = triangle[i][1];
   triangle_original[i] = temp;
@@ -38,8 +36,8 @@ scaleRotate.connect("value-changed", () => {
   // Recalculate value of points of triangle
   for (let i = 0; i < 3; i++) {
     // Calculate original angle
-    let x = triangle_original[i][0];
-    let y = triangle_original[i][1];
+    const x = triangle_original[i][0];
+    const y = triangle_original[i][1];
     let angle = Math.atan(Math.abs(y) / Math.abs(x));
     if (x > 0 && y > 0) {
       angle = angle;
@@ -72,7 +70,7 @@ scaleRotate.connect("value-changed", () => {
     // Add to original angle scale value
     angle += (scaleRotate.get_value() * Math.PI) / 180;
     // Set new value to triangle
-    let radius = Math.sqrt(x * x + y * y);
+    const radius = Math.sqrt(x * x + y * y);
 
     triangle[i][0] = radius * Math.cos(angle);
     triangle[i][1] = radius * Math.sin(angle);
