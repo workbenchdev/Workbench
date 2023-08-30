8e7d9a477318a34e2d6cdc6ac6f7c7ad39bd6ba8
Adeel Ahmed Qureshi <adeelahmedqureshi02@gmail.com>
2023-08-30 13:18:39 +0500

    Used glib.clone! in required pleces .

--- src/Library/demos/Overlay Split View/code.rs
+++ src/Library/demos/Overlay Split View/code.rs
@@ -1,27 +1,21 @@
+use crate::glib;
 use crate::workbench;
 use adw::prelude::*;
-use std::cell::RefCell;
-use std::rc::Rc;
 
 pub fn main() {
-    let overlay_split_view: Rc<RefCell<adw::OverlaySplitView>> = Rc::new(RefCell::new(
-        workbench::builder().object("split_view").unwrap(),
-    ));
+    let overlay_split_view: adw::OverlaySplitView =
+        workbench::builder().object("split_view").unwrap();
 
     let start_toggle: gtk::ToggleButton = workbench::builder().object("start_toggle").unwrap();
     let end_toggle: gtk::ToggleButton = workbench::builder().object("end_toggle").unwrap();
 
-    let overlay_split_view_clone = overlay_split_view.clone();
-    start_toggle.connect_toggled(move |_| {
-        overlay_split_view_clone
-            .borrow_mut()
+    start_toggle.connect_toggled(glib::clone!(@strong overlay_split_view => move |_| {
+        overlay_split_view
             .set_sidebar_position(gtk::PackType::Start);
-    });
+    }));
 
-    let overlay_split_view_clone = overlay_split_view.clone();
-    end_toggle.connect_toggled(move |_| {
-        overlay_split_view_clone
-            .borrow_mut()
+    end_toggle.connect_toggled(glib::clone!(@strong overlay_split_view =>move |_| {
+        overlay_split_view
             .set_sidebar_position(gtk::PackType::End);
-    });
+    }));
 }
