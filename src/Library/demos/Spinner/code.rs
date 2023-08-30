7efe6ab6970dcd1d6fa0ac1dede68d212d17e650
Onkar <121673391+onkarrai06@users.noreply.github.com>
2023-08-31 00:36:53 +0500

    Update code.rs

--- src/Library/demos/Spinner/code.rs
+++ src/Library/demos/Spinner/code.rs
@@ -6,7 +6,7 @@ pub fn main() {
     let spinner: gtk::Spinner = workbench::builder().object("spinner").unwrap();
 
     button.connect_clicked(move |button| {
-        if spinner.is_spinning() == true {
+        if spinner.is_spinning() {
             button.set_icon_name("media-playback-start");
             spinner.set_spinning(false);
         } else {
