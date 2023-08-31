5cde951d10fb9fe4d136f16dca2b02415fd77fb0
Julian Hofer <julianhofer@gnome.org>
2023-08-31 08:05:31 +0200

    Simplify code

--- src/Library/demos/Toggle Button/code.rs
+++ src/Library/demos/Toggle Button/code.rs
@@ -3,7 +3,7 @@ use gtk::prelude::*;
 use std::collections::HashMap;
 
 pub fn main() {
-    let buttons: HashMap<&str, &str> = HashMap::from([
+    let buttons = HashMap::from([
         ("button_no_look", "Don't look"),
         ("button_look", "Look"),
         ("button_camera", "Camera"),
@@ -13,10 +13,10 @@ pub fn main() {
 
     for (id, name) in buttons.into_iter() {
         let button: gtk::ToggleButton = workbench::builder().object(id).unwrap();
-        let cloned_name = name.to_string();
         button.connect_active_notify(move |button| {
             let status = if button.is_active() { "On" } else { "Off" };
-            println!("{cloned_name} {status}");
+            println!("{name} {status}");
         });
     }
 }
+
