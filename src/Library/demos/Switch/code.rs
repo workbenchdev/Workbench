2980d94cc97b90b28ef6d57e9d97935c21489296
onkarrai06 <onkarrai06@gmail.com>
2023-08-31 14:05:35 +0500

    library: Updated code for Switch in RUST

--- src/Library/demos/Switch/code.rs
+++ src/Library/demos/Switch/code.rs
@@ -1,7 +1,6 @@
 use crate::workbench;
 use glib::clone;
 use gtk::glib;
-use gtk::prelude::*;
 
 pub fn main() {
     let switch_on: gtk::Switch = workbench::builder().object("switch_on").unwrap();
@@ -10,17 +9,13 @@ pub fn main() {
     let switch_off: gtk::Switch = workbench::builder().object("switch_off").unwrap();
     let label_off: gtk::Label = workbench::builder().object("label_off").unwrap();
 
-    switch_on.connect_active_notify(clone!(@weak switch_off, @weak switch_on => move |_| {
-        label_on.set_label(if switch_on.is_active() {
-            "On"
-        } else {
-            "Off"
-        });
+    switch_on.connect_active_notify(clone!(@weak switch_off => move |switch_on| {
+        label_on.set_label(if switch_on.is_active() { "On" } else { "Off" });
         switch_off.set_active(!switch_on.is_active());
     }));
 
-    switch_off.connect_active_notify(clone!(@weak switch_off, @weak switch_on => move |_| {
+    switch_off.connect_active_notify(move |switch_off| {
         label_off.set_label(if switch_off.is_active() { "On" } else { "Off" });
         switch_on.set_active(!switch_off.is_active());
-    }));
+    });
 }
