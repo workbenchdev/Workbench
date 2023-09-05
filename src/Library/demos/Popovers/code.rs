918abdfb293eb32e41631a5400668c30d6b83772
Onkar <121673391+onkarrai06@users.noreply.github.com>
2023-09-06 00:35:36 +0500

    Update code.rs

--- src/Library/demos/Popovers/code.rs
+++ src/Library/demos/Popovers/code.rs
@@ -8,8 +8,8 @@ pub fn main() {
         let popover: gtk::Popover = workbench::builder().object(id).unwrap();
 
         popover.connect_closed(move |popover| {
-            let popover_name = popover.name();
-            println!("{} closed.", popover_name);
+            let popover_name = popover.widget_name();
+            println!("{popover_name} closed.")
         });
     }
 }
