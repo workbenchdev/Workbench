dc99a801854c6f0c7b3d70b0fec1c4855a884e26
Sabrina Meindlhumer <sabrina.meindlhumer@protonmail.com>
2023-09-08 21:46:22 +0200

    library: address comments

--- src/Library/demos/Calendar/code.rs
+++ src/Library/demos/Calendar/code.rs
@@ -1,25 +1,23 @@
 use crate::workbench;
-use glib::clone;
-use gtk::glib;
 
 pub fn main() {
     let calendar: gtk::Calendar = workbench::builder().object("calendar").unwrap();
 
-    calendar.connect_day_notify(clone!(@weak calendar => move |_| {
+    calendar.connect_day_notify(move |calendar| {
         println!("{}", calendar.date().format("%e").unwrap());
-    }));
+    });
 
-    calendar.connect_month_notify(clone!(@weak calendar => move |_| {
+    calendar.connect_month_notify(move |calendar| {
         println!("{}", calendar.date().format("%B").unwrap());
-    }));
+    });
 
-    calendar.connect_year_notify(clone!(@weak calendar => move |_| {
+    calendar.connect_year_notify(move |calendar| {
         println!("{}", calendar.date().format("%Y").unwrap());
-    }));
+    });
 
-    calendar.connect_day_selected(clone!(@weak calendar => move |_| {
+    calendar.connect_day_selected(move |calendar| {
         println!("{}", calendar.date().format_iso8601().unwrap());
-    }));
+    });
 
     calendar.mark_day(15);
 }
