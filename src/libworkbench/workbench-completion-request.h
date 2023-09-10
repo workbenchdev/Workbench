69d29563500630430f2d041b9cec1b6d58d60f3f
Andy Holmes <andrew.g.r.holmes@gmail.com>
2023-09-10 11:37:51 -0700

    fix: remove -fvisibility=hidden
    This is an internal library anyways and seemed to be causing some
    problems, or at least warnings, when compiling other parts of Workbench.

--- src/libworkbench/workbench-completion-request.h
+++ src/libworkbench/workbench-completion-request.h
@@ -31,27 +31,19 @@ typedef enum
 
 #define WORKBENCH_TYPE_COMPLETION_REQUEST (workbench_completion_request_get_type())
 
-WORKBENCH_EXPORT
 G_DECLARE_FINAL_TYPE (WorkbenchCompletionRequest, workbench_completion_request, WORKBENCH, COMPLETION_REQUEST, GObject)
 
-WORKBENCH_EXPORT
 GCancellable                * workbench_completion_request_get_cancellable (WorkbenchCompletionRequest  *request);
-WORKBENCH_EXPORT
 GtkSourceCompletionContext  * workbench_completion_request_get_context     (WorkbenchCompletionRequest  *request);
-WORKBENCH_EXPORT
 GtkSourceCompletionProvider * workbench_completion_request_get_provider    (WorkbenchCompletionRequest  *request);
-WORKBENCH_EXPORT
 WorkbenchRequestState         workbench_completion_request_get_state       (WorkbenchCompletionRequest  *request);
-WORKBENCH_EXPORT
 void                          workbench_completion_request_add             (WorkbenchCompletionRequest  *request,
                                                                             GtkSourceCompletionProposal *proposal);
-WORKBENCH_EXPORT
 void                          workbench_completion_request_splice          (WorkbenchCompletionRequest  *request,
                                                                             unsigned int                 position,
                                                                             unsigned int                 n_removals,
                                                                             gpointer                    *additions,
                                                                             unsigned int                 n_additions);
-WORKBENCH_EXPORT
 void                          workbench_completion_request_state_changed   (WorkbenchCompletionRequest  *request,
                                                                             WorkbenchRequestState        state);
 
