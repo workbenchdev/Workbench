e07a92ad9a49ef9d5291fd64c5e34de54aff6b66
Saad Khan <skhan.bee20seecs@seecs.edu.pk>
2023-09-15 21:00:19 +0500

    Updated `notification_count` to an integer value

--- src/Library/demos/View Switcher/code.rs
+++ src/Library/demos/View Switcher/code.rs
@@ -1,18 +1,17 @@
 use crate::workbench;
 use adw::prelude::*;
 
-use crate::glib::clone;
-use std::cell::Cell;
-use std::rc::Rc;
+use glib::clone;
+use gtk::glib;
 
 pub fn main() {
     let notifications_page: adw::ViewStackPage = workbench::builder().object("page3").unwrap();
     let notification_list: gtk::ListBox = workbench::builder().object("notification_list").unwrap();
 
-    let notification_count = Rc::new(Cell::new(5));
-    notifications_page.set_badge_number(notification_count.get());
+    let notification_count = 5;
+    notifications_page.set_badge_number(notification_count);
 
-    for _ in 0..notification_count.get() {
+    for _ in 0..notification_count {
         let notification_row = adw::ActionRow::builder()
             .title("Notification")
             .selectable(false)
@@ -29,8 +28,7 @@ pub fn main() {
         button.connect_clicked(clone!(
             @strong notification_count, @strong notifications_page,
             @strong notification_list, @strong notification_row => move |_| {
-                notification_count.set(notification_count.get() - 1);
-                notifications_page.set_badge_number(notification_count.get());
+                notifications_page.set_badge_number(notifications_page.badge_number() - 1);
                 notification_list.remove(&notification_row);
 
                 if notifications_page.badge_number() == 0 {
