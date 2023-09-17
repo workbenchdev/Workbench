56887ff99c6977edb4541d3840a6cb790801888d
Saad Khan <skhan.bee20seecs@seecs.edu.pk>
2023-09-17 21:44:11 +0500

    Changed indent to `2` to match other varients

--- src/Library/demos/View Switcher/code.rs
+++ src/Library/demos/View Switcher/code.rs
@@ -5,39 +5,39 @@ use glib::clone;
 use gtk::glib;
 
 pub fn main() {
-    let notifications_page: adw::ViewStackPage = workbench::builder().object("page3").unwrap();
-    let notification_list: gtk::ListBox = workbench::builder().object("notification_list").unwrap();
+  let notifications_page: adw::ViewStackPage = workbench::builder().object("page3").unwrap();
+  let notification_list: gtk::ListBox = workbench::builder().object("notification_list").unwrap();
 
-    let notification_count = 5;
-    notifications_page.set_badge_number(notification_count);
+  let notification_count = 5;
+  notifications_page.set_badge_number(notification_count);
 
-    for _ in 0..notification_count {
-        let notification_row = adw::ActionRow::builder()
-            .title("Notification")
-            .selectable(false)
-            .build();
+  for _ in 0..notification_count {
+    let notification_row = adw::ActionRow::builder()
+      .title("Notification")
+      .selectable(false)
+      .build();
 
-        let button = gtk::Button::builder()
-            .halign(gtk::Align::Center)
-            .valign(gtk::Align::Center)
-            .margin_top(10)
-            .margin_bottom(10)
-            .icon_name("check-plain-symbolic")
-            .build();
+    let button = gtk::Button::builder()
+      .halign(gtk::Align::Center)
+      .valign(gtk::Align::Center)
+      .margin_top(10)
+      .margin_bottom(10)
+      .icon_name("check-plain-symbolic")
+      .build();
 
-        button.connect_clicked(clone!(
-            @weak notifications_page, @weak notification_list, @weak notification_row => move |_| {
-                notifications_page.set_badge_number(notifications_page.badge_number() - 1);
-                notification_list.remove(&notification_row);
+    button.connect_clicked(clone!(
+      @weak notifications_page, @weak notification_list, @weak notification_row => move |_| {
+        notifications_page.set_badge_number(notifications_page.badge_number() - 1);
+        notification_list.remove(&notification_row);
 
-                if notifications_page.badge_number() == 0 {
-                    notifications_page.set_needs_attention(false);
-                }
-            }
-        ));
+        if notifications_page.badge_number() == 0 {
+          notifications_page.set_needs_attention(false);
+        }
+      }
+    ));
 
-        notification_row.add_suffix(&button);
+    notification_row.add_suffix(&button);
 
-        notification_list.append(&notification_row);
-    }
+    notification_list.append(&notification_row);
+  }
 }
