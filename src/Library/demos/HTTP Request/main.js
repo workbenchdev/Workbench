5d34072368d129a2ecf5fc1437e43684bee4ed11
Sonny Piers <sonny@fastmail.net>
2023-09-17 17:53:53 +0200

    improve

--- src/Library/demos/HTTP Request/main.js
+++ src/Library/demos/HTTP Request/main.js
@@ -3,73 +3,55 @@ import GLib from "gi://GLib";
 import Gio from "gi://Gio";
 
 Gio._promisify(Soup.Session.prototype, "send_async", "send_finish");
+Gio._promisify(Gio.OutputStream.prototype, "splice_async", "splice_finish");
 
 const http_session = new Soup.Session();
 const article_text_view = workbench.builder.get_object("article_text_view");
 const article_title = workbench.builder.get_object("article_title");
 
-async function fetchFeaturedArticle() {
-  try {
-    // Date
-    const date = new Date();
-    const year = date.getFullYear();
-    const month = String(date.getMonth() + 1).padStart(2, "0");
-    const day = String(date.getDate()).padStart(2, "0");
+fetchWikipediaTodaysFeaturedArticle().catch(console.error);
 
-    // Make request
-    const url = `https://api.wikimedia.org/feed/v1/wikipedia/en/featured/${year}/${month}/${day}`;
-    const message = Soup.Message.new("GET", url);
+async function fetchWikipediaTodaysFeaturedArticle() {
+  // https://gjs-docs.gnome.org/glib20~2.0/glib.datetime
+  const date = GLib.DateTime.new_now_local();
 
-    const input_stream = await http_session.send_async(
-      message,
-      GLib.PRIORITY_DEFAULT,
-      null,
-    );
+  // https://api.wikimedia.org/wiki/Feed_API/Reference/Featured_content
+  const language = "en";
+  const url = `https://api.wikimedia.org/feed/v1/wikipedia/${language}/featured/${date.format(
+    "%Y/%m/%d",
+  )}`;
+  const message = Soup.Message.new("GET", url);
 
-    if (message.status_code !== 200) {
-      article_title.label = "Failed to fetch data";
-      return;
-    }
-
-    const data = await inputStreamToString(input_stream);
-    const json = JSON.parse(data);
-
-    const text_buffer = article_text_view.get_buffer();
-    text_buffer.set_text(json.tfa.extract, -1);
-
-    const formatted_title = json.tfa.title
-      ? json.tfa.title.replace(/_/g, " ")
-      : "Today's Featured Article";
-
-    article_title.label = formatted_title;
-  } catch (error) {
-    article_title.label = "Failed to fetch data";
-    console.error("Error:", error);
-  }
-}
-
-async function inputStreamToString(inputStream) {
-  const dataInputStream = new Gio.DataInputStream({
-    base_stream: inputStream,
-  });
-
-  let data = "";
-  let [line, length] = await dataInputStream.read_line_async(
+  const input_stream = await http_session.send_async(
+    message,
     GLib.PRIORITY_DEFAULT,
     null,
   );
 
-  const textDecoder = new TextDecoder("utf-8"); // Initialize TextDecoder
-
-  while (line !== null) {
-    data += textDecoder.decode(line); // Decode the line to string
-    [line, length] = await dataInputStream.read_line_async(
-      GLib.PRIORITY_DEFAULT,
-      null,
-    );
+  if (message.status_code !== 200) {
+    console.error(`HTTP Status ${message.status_code}`);
+    return;
   }
-  return data;
+
+  const data = await readAsString(input_stream);
+  const json = JSON.parse(data);
+
+  article_text_view.buffer.set_text(json.tfa.extract, -1);
+  article_title.label = json.tfa.titles.normalized;
 }
 
-// Fetch the featured article
-fetchFeaturedArticle();
+async function readAsString(input_stream) {
+  const output_stream = Gio.MemoryOutputStream.new_resizable();
+
+  await output_stream.splice_async(
+    input_stream,
+    Gio.OutputStreamSpliceFlags.CLOSE_TARGET |
+      Gio.OutputStreamSpliceFlags.CLOSE_SOURCE,
+    GLib.PRIORITY_DEFAULT,
+    null,
+  );
+
+  const bytes = output_stream.steal_as_bytes();
+  const text_decoder = new TextDecoder("utf-8");
+  return text_decoder.decode(bytes.toArray().buffer);
+}
