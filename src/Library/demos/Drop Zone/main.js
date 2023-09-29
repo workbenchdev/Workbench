6734f8da5784f4ed674da2a71c0457ac6cb27a72
halfmexican <halfmexicanhalfamazing@gmail.com>
2023-09-29 09:33:54 -0500

    use async drop target

--- src/Library/demos/Drop Zone/main.js
+++ src/Library/demos/Drop Zone/main.js
@@ -5,44 +5,116 @@ import GObject from "gi://GObject";
 
 const bin = workbench.builder.get_object("bin");
 
-// Universal drop target for any String data
-const string_drop_target = Gtk.DropTarget.new(
-  GObject.TYPE_STRING,
-  Gdk.DragAction.COPY,
-);
+// Create a content formats builder and add multiple MIME types
+const content_formats_builder = Gdk.ContentFormatsBuilder.new();
+//content_formats_builder.add_gtype(GObject.TYPE_STRING);
+//content_formats_builder.add_mime_type("text/plain");
+content_formats_builder.add_mime_type("text/uri-list");
+const formats = content_formats_builder.to_formats();
 
-bin.add_controller(string_drop_target);
-
-string_drop_target.connect("drop", (self, value, x, y) => {
-  bin.child = createTextPreview(value);
-  bin.remove_css_class("overlay-drag-area");
+const generic_drop_target = new Gtk.DropTargetAsync({
+  actions: Gdk.DragAction.COPY,
+  formats: formats,
 });
 
-// Drop Target for Files
-const file_drop_target = Gtk.DropTarget.new(Gio.File, Gdk.DragAction.COPY);
-bin.add_controller(file_drop_target);
-file_drop_target.connect("drop", (self, value, x, y) => {
+bin.add_controller(generic_drop_target);
+
+// Connect drop event
+generic_drop_target.connect("drop", async (self, drop, x, y) => {
   try {
+<<<<<<< HEAD
+    // Read data asynchronously
+    const data = await new Promise((resolve, reject) => {
+      drop.read_value_async(
+        Gio.File, 
+        Gdk.PRIORITY_DEFAULT, // I/O priority
+        null, // GCancellable
+        (drop, res) => {
+          // Callback
+          try {
+            const actualValue = drop.read_value_finish(res);
+            if (actualValue) {
+              resolve(actualValue);
+            } else {
+              reject(new Error("Failed to read drop data"));
+            }
+          } catch (error) {
+            reject(error);
+          }
+        },
+      );
+    });
+
+    if (typeof data === "string") {
+      const formats = drop.get_formats();
+      const mime_types = formats.get_mime_types();
+
+      if (mime_types.includes("text/plain")) {
+        bin.child = createTextPreview(data);
+      } else if (mime_types.includes("text/uri-list")) {
+        const uris = data.split("\n");
+        const file = Gio.File.new_for_uri(uris[0].trim());
+        bin.child = await onDrop(file);
+      }
+    }
+
+    bin.remove_css_class("overlay-drag-area");
+    drop.finish(true);
+  } catch (error) {
+    console.log(error);
+    drop.finish(false);
+=======
     bin.child = onDrop(value);
   } catch (err) {
     console.error(err, "Unable to load preview");
+>>>>>>> main
   }
-  bin.remove_css_class("overlay-drag-area");
 });
 
-function onDrop(value) {
-  if (!(value instanceof Gio.File)) return false;
+async function onDrop(file) {
+  try {
+    // Log the file's URI for debugging
+    console.log("Dropped file URI:", file.get_uri());
 
-  const file_info = value.query_info("standard::content-type", 0, null);
-  const content_type = file_info.get_content_type();
+    // Log if the file exists
+    const exists = file.query_exists(null);
+    console.log("File exists:", exists);
 
-  if (content_type.startsWith("image/")) {
-    return createImagePreview(value);
-  } else if (content_type.startsWith("video/")) {
-    return createVideoPreview(value);
-  } else {
-    return createFilePreview(value);
+    // Query the file's information
+    const file_info = file.query_info(
+      "standard::type,standard::content-type",
+      0,
+      null,
+    );
+
+    const content_type = file_info.get_content_type();
+    const file_type = file_info.get_file_type();
+    console.log("Content Type:", content_type, "File Type:", file_type); // Log types for debugging
+
+    if (file_type === Gio.FileType.DIRECTORY) {
+      return createFolderPreview(file);
+    } else if (content_type.startsWith("image/")) {
+      return createImagePreview(file);
+    } else if (content_type.startsWith("video/")) {
+      return createVideoPreview(file);
+    } else {
+      return createFilePreview(file);
+    }
+  } catch (error) {
+    console.log("Error in onDrop:", error);
   }
+}
+
+function createFolderPreview(folder) {
+  const widget = createBoxWidget();
+
+  const folder_icon = Gtk.Image.new_from_icon_name("folder");
+  widget.append(folder_icon);
+
+  const folder_name = new Gtk.Label({ label: folder.get_basename() });
+  widget.append(folder_name);
+
+  return widget;
 }
 
 function createImagePreview(value) {
@@ -97,20 +169,13 @@ function createBoxWidget() {
   });
 }
 
-// Drop Hover Effect
+/* Drop Hover Effect
 
-file_drop_target.connect("enter", () => {
+generic_drop_target.connect("enter", () => {
   bin.add_css_class("overlay-drag-area");
 });
 
-file_drop_target.connect("leave", () => {
+generic_drop_target.connect("leave", () => {
   bin.remove_css_class("overlay-drag-area");
 });
-
-string_drop_target.connect("enter", () => {
-  bin.add_css_class("overlay-drag-area");
-});
-
-string_drop_target.connect("leave", () => {
-  bin.remove_css_class("overlay-drag-area");
-});
+*/
