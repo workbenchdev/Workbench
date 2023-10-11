18c4e492ff2c52f77b995f17b2e92362c01b3c5e
Julian Hofer <julianhofer@gnome.org>
2023-10-11 16:02:31 +0200

    rust: Clean target dir if rustc has been updated
    I've expected that the target dir will simply be cleaned with a new rustc version.
    Unfortunately that isn't what's happening here. So we have to do it ourselves.

--- src/langs/rust/Compiler.js
+++ src/langs/rust/Compiler.js
@@ -6,8 +6,23 @@ export default function Compiler({ session }) {
   const { file } = session;
   const cacheDir = GLib.get_user_cache_dir();
   const targetPath = `${cacheDir}/rust_build_cache`;
+  const rustcVersionFilePath = `${targetPath}/rustc_version.txt`;
 
   async function compile() {
+    // Check if the rustc version file exists and has the correct content
+    const rustcVersion = await getRustcVersion();
+    const decoder = new TextDecoder("utf-8");
+    if (
+      !GLib.file_test(rustcVersionFilePath, GLib.FileTest.EXISTS) ||
+      decoder.decode(GLib.file_get_contents(rustcVersionFilePath, null)[1]) !==
+        rustcVersion
+    ) {
+      // If the file doesn't exist or rustc version doesn't match, perform cleanup
+      await cargoClean();
+      GLib.mkdir_with_parents(targetPath, 0o755);
+      GLib.file_set_contents(rustcVersionFilePath, rustcVersion);
+    }
+
     const cargo_launcher = new Gio.SubprocessLauncher();
     cargo_launcher.set_cwd(file.get_path());
 
@@ -36,6 +51,28 @@ export default function Compiler({ session }) {
     }
 
     return true;
+  }
+
+  function getRustcVersion() {
+    const cargo_launcher = Gio.SubprocessLauncher.new(
+      Gio.SubprocessFlags.STDOUT_PIPE,
+    );
+    cargo_launcher.set_cwd(file.get_path());
+    const rustcVersionProcess = cargo_launcher.spawnv(["rustc", "--version"]);
+    const stdout = rustcVersionProcess.communicate_utf8(null, null)[1];
+    return stdout;
+  }
+
+  async function cargoClean() {
+    const cargo_launcher = new Gio.SubprocessLauncher();
+    cargo_launcher.set_cwd(file.get_path());
+    const cargoCleanProcess = cargo_launcher.spawnv([
+      "cargo",
+      "clean",
+      "--target-dir",
+      targetPath,
+    ]);
+    await cargoCleanProcess.wait_async(null);
   }
 
   return { compile, run };
