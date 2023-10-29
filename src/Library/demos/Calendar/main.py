d4bc328047a3b0de2ca5f3e0af7b44c78cfdecd0
Gregor Niehl <gregorniehl@web.de>
2023-10-29 13:57:17 +0100

    Library: Port 'Calendar' to Python

---
+++ src/Library/demos/Calendar/main.py
@@ -0,0 +1,24 @@
+import workbench
+
+calendar = workbench.builder.get_object("calendar")
+
+# calendar.get_date() returns a GLib.DateTime object
+# https://docs.gtk.org/glib/struct.DateTime.html
+
+calendar.connect("notify::day",
+    lambda *_: print(calendar.get_date().format("%e"))
+)
+
+calendar.connect("notify::month",
+    lambda *_:print(calendar.get_date().format("%B"))
+)
+
+calendar.connect("notify::year",
+    lambda *_: print(calendar.get_date().format("%Y"))
+)
+
+calendar.connect("day-selected",
+    lambda *_: print(calendar.get_date().format_iso8601())
+)
+
+calendar.mark_day(15)
