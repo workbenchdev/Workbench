f9e6a49144f23d50d979fe1c20e5ae4a1995b9c7
Marco Köpcke <hello@capypara.de>
2023-10-30 12:35:59 +0100

    python: Init GtkSource (#736)

--- src/Library/demos/Popovers/main.py
+++ src/Library/demos/Popovers/main.py
@@ -5,7 +5,7 @@ from gi.repository import Gtk
 import workbench
 
 
-def onClosed(popover):
+def on_closed(popover):
     name = popover.get_name()
     print(f"{name} closed.")
 
@@ -14,4 +14,4 @@ popover_ids = ("plain_popover", "popover_menu")
 
 for id in popover_ids:
     popover = workbench.builder.get_object(id)
-    popover.connect("closed", onClosed)
+    popover.connect("closed", on_closed)
