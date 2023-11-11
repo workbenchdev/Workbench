075ae98a6aa036bf4596b5f3ef84e496d3590124
Marco Capypara Köpcke <hello@capypara.de>
2023-11-11 17:28:25 +0100

    python: Minor styling changes to Text Colors demo

--- src/Library/demos/Text Colors/main.py
+++ src/Library/demos/Text Colors/main.py
@@ -10,15 +10,15 @@ import workbench
 label: Gtk.Label = workbench.builder.get_object("label")
 
 
-def updateAttributes():
+def update_attributes():
     # A Pango Attribute List is used to style the label
-    label.set_attributes(rainbowAttributes(label.get_label()))
+    label.set_attributes(rainbow_attributes(label.get_label()))
 
 
 # Generates an Attribute List that styles the label in rainbow colors.
 # The `text` parameter is needed to detect string length + position of spaces
-def rainbowAttributes(text):
-    RAINBOW_COLORS = (
+def rainbow_attributes(text):
+    rainbow_colors = (
         "#D00",
         "#C50",
         "#E90",
@@ -29,32 +29,31 @@ def rainbowAttributes(text):
     )
 
     # Create a color array with the length needed to color all the letters
-    colorArray = []
+    color_array = []
     i = 0
     while i < len(text):
-        colorArray += RAINBOW_COLORS
-        i = len(colorArray)
+        color_array += rainbow_colors
+        i = len(color_array)
 
     # Independent variable from `i` in the following loop to avoid spaces "consuming" a color
-    colorIdx = 0
+    color_idx = 0
 
-    attrListString = ""
+    attr_list_string = ""
     for i in range(len(text)):
         # Skip space characters
         if text[i] != " ":
-            startIdx = i
-            endIdx = i + 1
+            start_idx = i
+            end_idx = i + 1
 
-            color = colorArray[colorIdx]
-            colorIdx += 1
+            color = color_array[color_idx]
+            color_idx += 1
             # See comment below
-            attrListString += f"{startIdx} {endIdx} foreground {color},"
+            attr_list_string += f"{start_idx} {end_idx} foreground {color},"
 
     # For more info about the syntax for this function, see:
     # https://docs.gtk.org/Pango/method.AttrList.to_string.html
-    print(attrListString)
-    return Pango.attr_list_from_string(attrListString)
+    return Pango.attr_list_from_string(attr_list_string)
 
 
-label.connect("notify::label", lambda _, __: updateAttributes())
-updateAttributes()
+label.connect("notify::label", lambda _, __: update_attributes())
+update_attributes()
