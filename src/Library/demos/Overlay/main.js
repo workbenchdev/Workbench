b0d5bfeb33b5c56ecbb762fa21b46796087eb7ab
Sonny Piers <sonny@fastmail.net>
2023-12-09 11:52:55 +0100

    Move demos to a separate repository (#843)

--- src/Library/demos/Overlay/main.js
+++
@@ -1,6 +0,0 @@
-import Gio from "gi://Gio";
-
-const file = Gio.File.new_for_uri(workbench.resolve("./image.png"));
-
-const picture = workbench.builder.get_object("picture");
-picture.file = file;
--- src/Library/demos/Separator/main.js
+++
@@ -1,9 +0,0 @@
-import Gio from "gi://Gio";
-
-const picture_one = workbench.builder.get_object("picture_one");
-const picture_two = workbench.builder.get_object("picture_two");
-
-const file = Gio.File.new_for_uri(workbench.resolve("./image.png"));
-
-picture_one.file = file;
-picture_two.file = file;
