f4770d7df1c40acefc7dd7dd63899455ac410ecf
Sonny Piers <sonny@fastmail.net>
2023-12-09 11:20:50 +0100

    Move demos to a separate repository

--- src/Library/demos/Breakpoints/main.js
+++
@@ -1,9 +0,0 @@
-const breakpoint = workbench.builder.get_object("breakpoint");
-
-breakpoint.connect("apply", () => {
-  console.log("Breakpoint Applied");
-});
-
-breakpoint.connect("unapply", () => {
-  console.log("Breakpoint Unapplied");
-});
