04f65cdc288934477fdea3f50091b504d6c86394
Sonny Piers <sonny@fastmail.net>
2024-02-21 20:13:37 +0100

    46 beta (#896)

--- test/init.js
+++
@@ -1,9 +0,0 @@
-// FIXME: use src/init.js
-// breaks `make test`, we need to run tests from Workbench sandbox
-
-import "gi://Gtk?version=4.0";
-import "gi://GtkSource?version=5";
-import "gi://Adw?version=1";
-import "gi://Soup?version=3.0";
-import "gi://WebKit?version=6.0";
-import "gi://Pango?version=1.0";
