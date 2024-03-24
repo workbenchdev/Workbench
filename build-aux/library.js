b295055d31d68415d8535f23bec6ee018fc62fe5
Sonny Piers <sonny@fastmail.net>
2024-03-24 23:11:24 +0100

    f

--- build-aux/library.js
+++ build-aux/library.js
@@ -3,6 +3,8 @@
 import Gio from "gi://Gio";
 import GLib from "gi://GLib";
 
+const loop = new GLib.MainLoop(null, false);
+
 Gio._promisify(
   Gio.File.prototype,
   "enumerate_children_async",
@@ -18,74 +20,94 @@ Gio._promisify(
 Gio._promisify(Gio.File.prototype, "copy_async", "copy_finish");
 
 const [pkgdatadir] = ARGV;
-GLib.mkdir_with_parents(pkgdatadir, 0o755);
+GLib.mkdir_with_parents(
+  Gio.File.new_for_path(pkgdatadir).get_child("demos").get_path(),
+  0o755,
+);
 
 const demos_dir = Gio.File.new_for_path(
   GLib.getenv("MESON_SOURCE_ROOT"),
 ).get_child("demos/src");
 const demos = [];
 
-const enumerator = await demos_dir.enumerate_children_async(
-  `${Gio.FILE_ATTRIBUTE_STANDARD_NAME},${Gio.FILE_ATTRIBUTE_STANDARD_IS_HIDDEN}`,
-  Gio.FileQueryInfoFlags.NOFOLLOW_SYMLINKS,
-  GLib.PRIORITY_DEFAULT,
-  null,
-);
-for (const file_info of enumerator) {
-  if (file_info.get_is_hidden()) continue;
-  if (file_info.get_file_type() !== Gio.FileType.DIRECTORY) continue;
+(async () => {
+  const enumerator = await demos_dir.enumerate_children_async(
+    `${Gio.FILE_ATTRIBUTE_STANDARD_NAME},${Gio.FILE_ATTRIBUTE_STANDARD_IS_HIDDEN}`,
+    Gio.FileQueryInfoFlags.NOFOLLOW_SYMLINKS,
+    GLib.PRIORITY_DEFAULT,
+    null,
+  );
+  for (const file_info of enumerator) {
+    if (file_info.get_is_hidden()) continue;
+    if (file_info.get_file_type() !== Gio.FileType.DIRECTORY) continue;
 
-  const demo_dir = enumerator.get_child(file_info);
+    const demo_dir = enumerator.get_child(file_info);
 
-  let str;
-  try {
-    str = new TextDecoder().decode(
-      demo_dir.get_child("main.json").load_contents(null)[1],
+    let str;
+    try {
+      str = new TextDecoder().decode(
+        demo_dir.get_child("main.json").load_contents(null)[1],
+      );
+    } catch (err) {
+      console.warn(err);
+      continue;
+    }
+
+    const demo = JSON.parse(str);
+    demo.name = file_info.get_name();
+
+    const languages = [];
+    if (demo_dir.get_child("main.js").query_exists(null)) {
+      languages.push("javascript");
+    }
+    if (demo_dir.get_child("main.vala").query_exists(null)) {
+      languages.push("vala");
+    }
+    if (demo_dir.get_child("code.rs").query_exists(null)) {
+      languages.push("rust");
+    }
+    if (demo_dir.get_child("main.py").query_exists(null)) {
+      languages.push("python");
+    }
+    demo.languages = languages;
+
+    console.log(
+      Gio.File.new_for_path(pkgdatadir)
+        .get_child("demos")
+        .get_child(demo_dir.get_basename())
+        .get_path(),
     );
-  } catch (err) {
-    console.warn(err);
-    continue;
+
+    await copyDemo(
+      demo_dir,
+      Gio.File.new_for_path(pkgdatadir)
+        .get_child("demos")
+        .get_child(demo_dir.get_basename()),
+    );
+
+    demos.push(demo);
   }
 
-  const demo = JSON.parse(str);
-  demo.name = file_info.get_name();
+  await Gio.File.new_for_path(pkgdatadir)
+    .get_child("demos/index.json")
+    .replace_contents_async(
+      new TextEncoder().encode(JSON.stringify(demos)),
+      null,
+      false,
+      Gio.FileCreateFlags.NONE,
+      null,
+    );
+})()
+  .catch((err) => {
+    throw err;
+  })
+  .finally(() => {
+    loop.quit();
+  });
 
-  const languages = [];
-  if (demo_dir.get_child("main.js").query_exists(null)) {
-    languages.push("javascript");
-  }
-  if (demo_dir.get_child("main.vala").query_exists(null)) {
-    languages.push("vala");
-  }
-  if (demo_dir.get_child("code.rs").query_exists(null)) {
-    languages.push("rust");
-  }
-  if (demo_dir.get_child("main.py").query_exists(null)) {
-    languages.push("python");
-  }
-  demo.languages = languages;
+loop.run();
 
-  await copyDirectory(
-    demo_dir,
-    Gio.File.new_for_path(pkgdatadir)
-      .get_child("demos")
-      .get_child(demo_dir.get_basename()),
-  );
-
-  demos.push(demo);
-}
-
-await Gio.File.new_for_path(pkgdatadir)
-  .get_child("demos/index.json")
-  .replace_contents_async(
-    new TextEncoder().encode(JSON.stringify(demos)),
-    null,
-    false,
-    Gio.FileCreateFlags.NONE,
-    null,
-  );
-
-export async function copyDirectory(source, destination) {
+async function copyDemo(source, destination) {
   const enumerator = await source.enumerate_children_async(
     `${Gio.FILE_ATTRIBUTE_STANDARD_NAME},${Gio.FILE_ATTRIBUTE_STANDARD_IS_HIDDEN}`,
     Gio.FileQueryInfoFlags.NOFOLLOW_SYMLINKS,
