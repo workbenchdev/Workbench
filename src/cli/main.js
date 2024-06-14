d8f06f1b46057506c49f0ec4a3e1a00fc018dc9e
Sonny Piers <sonny@fastmail.net>
2024-06-15 00:23:15 +0200

    cli: Filter out erronous Vala CSS Gradients deprecations

--- src/cli/main.js
+++ src/cli/main.js
@@ -393,6 +393,16 @@ async function ci({ filenames, current_dir }) {
         diagnostics = diagnostics.filter((diagnostic) => {
           return !ignore_for_text_fields.includes(diagnostic.message);
         });
+        // Gtk.StyleContext class is deprecated but not the following methods
+        // gtk_style_context_add_provider_for_display
+        // gtk_style_context_remove_provider_for_display
+      } else if (demo_dir.get_basename() === "CSS Gradients") {
+        diagnostics = diagnostics.filter((diagnostic) => {
+          return (
+            diagnostic.message !==
+            "`Gtk.StyleContext' has been deprecated since 4.10"
+          );
+        });
       }
 
       if (diagnostics.length > 0) {
